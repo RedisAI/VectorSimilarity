94079ddb17c6e980cb6ab6c28897fdc6a5275059
DvirDukhan <dvir@redislabs.com>
2021-07-28 22:31:00 +0300

    gtest in cmake

--- tests/unit/test_hnswlib.cpp
+++ tests/unit/test_hnswlib.cpp
@@ -311,3 +311,9 @@ TEST_F(HNSWLibTest, sanity_rinsert_1280) {
     }
     VecSimIndex_Free(index);
 }
+
+
+int main(int argc, char **argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
