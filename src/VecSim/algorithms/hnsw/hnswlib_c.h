65f0a445d0b5958b522e35b17e92da36ad8f5878
alon <alonreshef24@gmail.com>
2021-08-09 12:38:31 +0300

    merge with main branch

---
+++ src/VecSim/algorithms/hnsw/hnswlib_c.h
@@ -0,0 +1,33 @@
+
+#pragma once
+
+#include <cstdlib>
+#include "VecSim/vecsim.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+struct HNSWIndex;
+
+VecSimIndex *HNSWLib_New(const VecSimParams *params);
+
+void HNSWLib_Free(VecSimIndex *index);
+
+int HNSWLib_AddVector(VecSimIndex *index, const void *vector_data, size_t id);
+
+int HNSWLib_DeleteVector(VecSimIndex *index, size_t id);
+
+size_t HNSWLib_Size(VecSimIndex *index);
+
+VecSimQueryResult *HNSWLib_TopKQuery(VecSimIndex *index, const void *queryBlob, size_t k,
+                                  VecSimQueryParams *queryParams);
+
+// TODO
+
+VecSimQueryResult *HNSWLib_DistanceQuery(VecSimIndex *index, const void *queryBlob, float distance,
+                                      VecSimQueryParams queryParams);
+
+#ifdef __cplusplus
+}
+#endif
