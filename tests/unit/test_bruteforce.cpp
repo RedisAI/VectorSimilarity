f4dabbb76639f1c1b880ae9ed7eeb580ab924e56
DvirDukhan <dvir@redislabs.com>
2021-08-10 12:21:00 +0300

    ip test

--- tests/unit/test_bruteforce.cpp
+++ tests/unit/test_bruteforce.cpp
@@ -29,7 +29,7 @@ TEST_F(BruteForceTest, brute_force_vector_add_test) {
     VecSimIndex_Free(index);
 }
 
-TEST_F(BruteForceTest, brute_force_vector_search_test) {
+TEST_F(BruteForceTest, brute_force_vector_search_test_ip) {
     VecSimParams params = {
         bfParams : {initialCapacity : 200},
         type : VecSimType_FLOAT32,
@@ -48,13 +48,14 @@ TEST_F(BruteForceTest, brute_force_vector_search_test) {
     ASSERT_EQ(VecSimIndex_IndexSize(index), n);
 
     float query[4] = {50, 50, 50, 50};
-    size_t ids[55] = {0};
+    size_t ids[100] = {0};
     VecSimQueryResult *res = VecSimIndex_TopKQuery(index, (const void *)query, k, NULL);
     ASSERT_EQ(VecSimQueryResult_Len(res), k);
     for (int i = 0; i < k; i++) {
         ids[res[i].id] = res[i].id;
+        std::cout<<res[i].id<<std::endl;
     }
-    for(size_t i = 46; i < 55; i++) {
+    for(size_t i = 89; i < 99; i++) {
         ASSERT_EQ(i, ids[i]);
     }
     VecSimQueryResult_Free(res);
