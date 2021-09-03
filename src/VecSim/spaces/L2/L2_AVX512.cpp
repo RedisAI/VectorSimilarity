cf60d1e6273058eb5c8ca5a1206275f9adfc70d7
DvirDukhan <dvir@redislabs.com>
2021-09-03 19:48:41 +0300

    fixed l2 avx512

--- src/VecSim/spaces/L2/L2_AVX512.cpp
+++ src/VecSim/spaces/L2/L2_AVX512.cpp
@@ -1,7 +1,7 @@
 #include "L2_AVX512.h"
 #include "L2.h"
 #include "VecSim/spaces/space_includes.h"
-#ifdef __AVX512__
+#ifdef __AVX512F__
 
 float L2SqrSIMD16Ext_AVX512(const void *pVect1v, const void *pVect2v, const void *qty_ptr) {
     float *pVect1 = (float *)pVect1v;
