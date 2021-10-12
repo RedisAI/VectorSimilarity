ac743b33ae361a2641cc224c3456e913b3e04fe8
DvirDukhan <dvir@redislabs.com>
2021-10-12 10:25:58 +0300

    sanity tests

--- tests/flow/test_bruteforce.py
+++ tests/flow/test_bruteforce.py
@@ -6,10 +6,9 @@ import time
 
 
 
-def test_bf():
+def test_bf_cosine():
     dim = 128
     num_elements = 1000000
-    num_queries = 10
     k=10
 
     bfparams = BFParams()
@@ -24,17 +23,51 @@ def test_bf():
         bfindex.add_vector(vector, i)
         vectors.append((i, vector))
 
-    query_data = np.float32(np.random.random((num_queries, dim)))
+    query_data = np.float32(np.random.random((1, dim)))
 
-    for vector in query_data:
-            dists = [(spatial.distance.cosine(vector, vec), key) for key, vec in vectors]
-            dists = sorted(dists)[:k]
-            keys = [key for _, key in dists[:k]]
-            dists = [dist for dist, _ in dists[:k]]
-            start = time.time()
-            bf_labels, bf_distances = bfindex.knn_query(vector, k=10)
-            end = time.time()
-            print("lookup time for 1M *128 vectors took " + str(end - start) + " seconds")
 
-            assert_allclose(bf_labels, [keys],  rtol=1e-5, atol=0)
-            assert_allclose(bf_distances, [dists],  rtol=1e-5, atol=0)
+    dists = [(spatial.distance.cosine(query_data, vec), key) for key, vec in vectors]
+    dists = sorted(dists)[:k]
+    keys = [key for _, key in dists[:k]]
+    dists = [dist for dist, _ in dists[:k]]
+    start = time.time()
+    bf_labels, bf_distances = bfindex.knn_query(query_data, k=10)
+    end = time.time()
+    print("lookup time for 1M *128 vectors took " + str(end - start) + " seconds")
+
+    assert_allclose(bf_labels, [keys],  rtol=1e-5, atol=0)
+    assert_allclose(bf_distances, [dists],  rtol=1e-5, atol=0)
+
+
+def test_bf_l2():
+    dim = 128
+    num_elements = 1000000
+    num_queries = 10
+    k=10
+
+    bfparams = BFParams()
+    bfparams.initialCapacity = num_elements
+    bfparams.blockSize = num_elements
+    bfindex = BFIndex(bfparams, VecSimType_FLOAT32, dim, VecSimMetric_L2)
+
+    data = np.float32(np.random.random((num_elements, dim)))
+    vectors = []
+
+    for i, vector in enumerate(data):
+        bfindex.add_vector(vector, i)
+        vectors.append((i, vector))
+
+    query_data = np.float32(np.random.random((1, dim)))
+
+
+    dists = [(spatial.distance.euclidean(query_data, vec), key) for key, vec in vectors]
+    dists = sorted(dists)[:k]
+    keys = [key for _, key in dists[:k]]
+    dists = [dist for dist, _ in dists[:k]]
+    start = time.time()
+    bf_labels, bf_distances = bfindex.knn_query(query_data, k=10)
+    end = time.time()
+    print("lookup time for 1M *128 vectors took " + str(end - start) + " seconds")
+
+    assert_allclose(bf_labels, [keys],  rtol=1e-5, atol=0)
+
--- tests/flow/test_hnswlib.py
+++ tests/flow/test_hnswlib.py
@@ -6,14 +6,13 @@ from  numpy.testing import assert_allclose
 
 
 # compare results with the original version of hnswlib - do not use elements deletion.
-def test_sanity_hnswlib_index():
+def test_sanity_hnswlib_index_L2():
     dim = 16
     num_elements = 10000
     space = 'l2'
     M=16
     efConstruction = 100
 
-    num_queries = 10
     efRuntime = 10
 
     params = VecSimParams()
@@ -41,12 +40,52 @@ def test_sanity_hnswlib_index():
         index.add_vector(vector, i)
         p.add_items(vector, i)
 
-    query_data = np.float32(np.random.random((num_queries, dim)))
-    for vector in query_data:
-            hnswlib_labels, hnswlib_distances = p.knn_query(vector, k=10)
-            redis_labels, redis_distances = index.knn_query(vector, 10)
-            assert_allclose(hnswlib_labels, redis_labels,  rtol=1e-5, atol=0)
-            assert_allclose(hnswlib_distances, redis_distances,  rtol=1e-5, atol=0)
+    query_data = np.float32(np.random.random((1, dim)))
+    hnswlib_labels, hnswlib_distances = p.knn_query(query_data, k=10)
+    redis_labels, redis_distances = index.knn_query(query_data, 10)
+    assert_allclose(hnswlib_labels, redis_labels,  rtol=1e-5, atol=0)
+    assert_allclose(hnswlib_distances, redis_distances,  rtol=1e-5, atol=0)
+
+def test_sanity_hnswlib_index_cosine():
+    dim = 16
+    num_elements = 10000
+    space = 'cosine'
+    M=16
+    efConstruction = 100
+
+    efRuntime = 10
+
+    params = VecSimParams()
+    hnswparams = HNSWParams()
+
+    params.algo = VecSimAlgo_HNSWLIB
+    params.dim = dim
+    params.metric = VecSimMetric_Cosine
+    params.type = VecSimType_FLOAT32
+
+    hnswparams.M = M
+    hnswparams.efConstruction = efConstruction
+    hnswparams.initialCapacity = num_elements
+    hnswparams.efRuntime=efRuntime
+
+    params.hnswParams = hnswparams
+    index = VecSimIndex(params)
+
+    p = hnswlib.Index(space=space, dim=dim)
+    p.init_index(max_elements=num_elements, ef_construction=efConstruction, M=M)
+    p.set_ef(efRuntime)
+
+    data = np.float32(np.random.random((num_elements, dim)))
+    for i, vector in enumerate(data):
+        index.add_vector(vector, i)
+        p.add_items(vector, i)
+
+    query_data = np.float32(np.random.random((1, dim)))
+    hnswlib_labels, hnswlib_distances = p.knn_query(query_data, k=10)
+    redis_labels, redis_distances = index.knn_query(query_data, 10)
+    assert_allclose(hnswlib_labels, redis_labels,  rtol=1e-5, atol=0)
+    assert_allclose(hnswlib_distances, redis_distances,  rtol=1e-5, atol=0)
+
 
 
 # Validate correctness of delete implementation comparing the brute force search. We test the search recall which is not
