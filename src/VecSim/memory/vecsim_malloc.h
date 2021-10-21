6bd26e1b848f5a34db01fa2e301abbc88c92b212
DvirDukhan <dvir@redis.com>
2021-10-21 11:25:00 +0300

    added vecsim_malloc

---
+++ src/VecSim/memory/vecsim_malloc.h
@@ -0,0 +1,41 @@
+#pragma once
+
+#include "redismodule.h"
+#include <stddef.h>
+
+#ifdef REDIS_MODULE_TARGET /* Set this when compiling your code as a module */
+static inline void *vecsim_malloc(size_t n) {
+	return RedisModule_Alloc(n);
+}
+static inline void *vecsim_calloc(size_t nelem, size_t elemsz) {
+	return RedisModule_Calloc(nelem, elemsz);
+}
+static inline void *vecsim_realloc(void *p, size_t n) {
+	return RedisModule_Realloc(p, n);
+}
+static inline void vecsim_free(void *p) {
+	RedisModule_Free(p);
+}
+static inline char *vecsim_strdup(const char *s) {
+	return RedisModule_Strdup(s);
+}
+
+static inline char *vecsim_strndup(const char *s, size_t n) {
+	char *ret = (char *)rm_malloc(n + 1);
+
+	if(ret) {
+		ret[n] = '\0';
+		memcpy(ret, s, n);
+	}
+	return ret;
+}
+#else
+
+#define vecsim_malloc malloc
+#define vecsim_free free
+#define vecsim_calloc calloc
+#define vecsim_realloc realloc
+#define vecsim_strdup strdup
+#define vecsim_strndup strndup
+
+#endif
