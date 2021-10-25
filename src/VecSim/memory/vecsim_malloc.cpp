a99a1a66f004f5c7c81e7c51e1d802ecd6639d2c
DvirDukhan <dvir@redis.com>
2021-10-25 12:55:07 +0300

    base vecsim object

---
+++ src/VecSim/memory/vecsim_malloc.cpp
@@ -0,0 +1,12 @@
+#include "vecsim_malloc.h"
+
+void * VecsimBaseObject::operator new(size_t size)
+    {
+        void * p = vecsim_malloc(size);
+        return p;
+    }
+ 
+void VecsimBaseObject::operator delete(void * p)
+    {
+        vecsim_free(p);
+    }
