20e04e71fc615b156fee3a2cff9d518eba4d8af7
alon <alonreshef24@gmail.com>
2021-11-09 13:40:31 +0200

    Add time measurement

--- tests/unit/bf_benchmark.cpp
+++ tests/unit/bf_benchmark.cpp
@@ -1,5 +1,6 @@
 #include <random>
 #include <iostream>
+#include <chrono>
 #include "VecSim/vec_sim.h"
 #include "VecSim/query_results.h"
 
@@ -34,7 +35,8 @@ public:
         std::cout << "set-up is done" << std::endl;
     }
 
-    void get_10000_total_results(size_t res_per_iter) {
+    long long get_10000_total_results(size_t res_per_iter) {
+        long long search_time = 0;
         std::vector<float> query(dim);
         std::uniform_real_distribution<> distrib;
         for (size_t i = 0; i < dim; ++i) {
@@ -43,12 +45,16 @@ public:
         VecSimBatchIterator *batchIterator = VecSimBatchIterator_New(bf_index, query.data());
         size_t res_num = 0;
         while (VecSimBatchIterator_HasNext(batchIterator)) {
+            auto start = std::chrono::high_resolution_clock::now();
             VecSimQueryResult_List res = VecSimBatchIterator_Next(batchIterator, res_per_iter, BY_SCORE);
+            auto elapsed = std::chrono::high_resolution_clock::now() - start;
+            search_time += std::chrono::duration_cast<std::chrono::microseconds>(elapsed).count();
             res_num += VecSimQueryResult_Len(res);
-            if (res_num == 1000) {
+            if (res_num == 10000) {
                 break;
             }
         }
+        return search_time;
     }
 
     void TearDown() {
@@ -59,8 +65,13 @@ public:
 int main() {
     BM_BatchIteratorBF bm;
     bm.SetUp();
-    for (size_t i=0; i<100; i++) {
-        bm.get_10000_total_results(1000);
+    long long total_time = 0;
+    size_t res_per_iter = 100;
+    size_t n = 10;
+    for (size_t i=0; i<n; i++) {
+        total_time += bm.get_10000_total_results(res_per_iter);
     }
+    std::cout << "Avg time for " << res_per_iter << " results per iteration is: " << (double) total_time/n/1000000
+    << " seconds" << std::endl;
     bm.TearDown();
 }
