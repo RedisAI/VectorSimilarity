d0993b73738de879ba4d14920d67215502fb6f2e
alon <alonreshef24@gmail.com>
2021-11-18 16:15:01 +0200

    Finalize flow test + small fix to sorting by score

--- tests/flow/test_bruteforce.py
+++ tests/flow/test_bruteforce.py
@@ -80,19 +80,37 @@ def test_batch_iterator():
 
     # Add 1M random vectors to the index
     data = np.float32(np.random.random((num_elements, dim)))
-    vectors = []
     for i, vector in enumerate(data):
         bf_index.add_vector(vector, i)
-        vectors.append((i, vector))
 
-    # Create a random query vector
+    # Create a random query vector and create a batch iterator
     query_data = np.float32(np.random.random((1, dim)))
-    #print("query data: ", query_data)
-    # Create batch iterator for this query vector
     batch_iterator = BFBatchIterator(bf_index, query_data)
-    labels, distances = batch_iterator.get_next_results(10, BY_ID)
-    print (labels)
-    print(distances)
-    labels, distances = batch_iterator.get_next_results(10, BY_ID)
-    print (labels)
-    print(distances)
+    labels_first_batch, distances_first_batch = batch_iterator.get_next_results(10, BY_ID)
+    for i, _ in enumerate(labels_first_batch[0][:-1]):
+        # assert sorting by id
+        assert(labels_first_batch[0][i] < labels_first_batch[0][i+1])
+
+    labels_second_batch, distances_second_batch = batch_iterator.get_next_results(10, BY_SCORE)
+    for i, dist in enumerate(distances_second_batch[0][:-1]):
+        # assert sorting by score
+        assert(distances_second_batch[0][i] < distances_second_batch[0][i+1])
+        # assert that every distance in the second batch is higher than any distance of the first batch
+        assert(len(distances_first_batch[0][np.where(distances_first_batch[0] > dist)]) == 0)
+
+    # reset
+    batch_iterator.reset()
+
+    # Run again in batches until depleted
+    batch_size = 1500
+    returned_results_num = 0
+    iterations = 0
+    start = time.time()
+    while batch_iterator.has_next():
+        iterations += 1
+        labels, distances = batch_iterator.get_next_results(batch_size, BY_SCORE)
+        returned_results_num += len(labels[0])
+
+    print(f'Total search time for running batches of size {batch_size} for index with {num_elements} of dim={dim}: {time.time() - start}')
+    assert (returned_results_num == num_elements)
+    assert (iterations == np.ceil(num_elements/batch_size))
