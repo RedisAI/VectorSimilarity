711ade4140ba2667ffeeec2faf8b1efed72ebd7b
rafie <rafi@redislabs.com>
2021-12-09 19:33:43 +0200

    fixes 13

--- tests/flow/common.py
+++ tests/flow/common.py
@@ -3,10 +3,3 @@ import numpy as np
 from scipy import spatial
 from  numpy.testing import assert_allclose
 import time
-import os, sys
-
-HERE = os.path.abspath(os.path.dirname(__file__))
-ROOT = os.path.abspath(os.path.join(HERE, "../.."))
-READIES = os.path.join(ROOT, "deps/readies")
-sys.path.insert(0, READIES)
-import paella
