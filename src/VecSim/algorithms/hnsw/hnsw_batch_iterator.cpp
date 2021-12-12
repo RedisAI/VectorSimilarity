4335ce2f1517a3ff052a0936f7b22f911169afeb
alon <alonreshef24@gmail.com>
2021-12-12 13:14:31 +0200

    WIP - before testing

--- src/VecSim/algorithms/hnsw/hnsw_batch_iterator.cpp
+++ src/VecSim/algorithms/hnsw/hnsw_batch_iterator.cpp
@@ -2,11 +2,20 @@
 #include "VecSim/query_result_struct.h"
 #include "VecSim/algorithms/hnsw/visited_list_pool.h"
 
-bool HNSW_BatchIterator::hasReturned(idType node_id) const {
-    return this->visited_list[node_id] == this->c
+inline bool HNSW_BatchIterator::hasReturned(idType node_id) const {
+    return this->visited_list->visitedElements[node_id] == this->tag_range_start+RETURNED_VISITED_ODD_OFFSET
+    || this->visited_list->visitedElements[node_id] == this->tag_range_start+RETURNED_VISITED_EVEN_OFFSET;
 }
 
-CandidatesHeap HNSW_BatchIterator::scanGraph() {
+inline void HNSW_BatchIterator::markReturned (uint node_id) {
+    this->visited_list->visitedElements[node_id] = this->cur_returned_visited_tag;
+}
+
+inline void HNSW_BatchIterator::unmarkReturned (uint node_id) {
+    this->visited_list->visitedElements[node_id] = this->cur_visited_tag;
+}
+
+unique_ptr<CandidatesHeap> HNSW_BatchIterator::scanGraph() {
 
     CandidatesHeap top_candidates(this->allocator);
     CandidatesHeap candidate_set(this->allocator);
@@ -15,13 +24,16 @@ CandidatesHeap HNSW_BatchIterator::scanGraph() {
     auto space = this->index->space.get();
 
     hnswlib::vl_type *visited_array = this->visited_list->visitedElements;
-    // Replace the visited tag to be the different one than the previous iteration tag.
-    hnswlib::vl_type cur_visited_tag = this->visited_tag == this->minimal_tag ? this->minimal_tag+1 :
-                                       this->minimal_tag;
-    hnswlib::vl_type cur_returned_and_visited_tag = this->visited_and_returned_tag == this->minimal_tag+2 ?
-            this->minimal_tag+2 : this->minimal_tag+3;
-    auto dist_func = space->get_dist_func();
 
+    // Replace the visited tag and returned_visited rags to be the different one than the previous iteration.
+    this->cur_visited_tag = (this->cur_visited_tag == this->tag_range_start+VISITED_ODD_OFFSET) ?
+                                       this->tag_range_start+VISITED_EVEN_OFFSET :
+                                       this->tag_range_start+VISITED_ODD_OFFSET;
+    this->cur_returned_visited_tag = (this->cur_returned_visited_tag == this->tag_range_start+RETURNED_VISITED_ODD_OFFSET) ?
+            this->tag_range_start+RETURNED_VISITED_EVEN_OFFSET :
+            this->tag_range_start+RETURNED_VISITED_ODD_OFFSET;
+
+    auto dist_func = space->get_dist_func();
     float dist = dist_func(this->getQueryBlob(), hnsw_index.getDataByInternalId(this->entry_point),
                            space->get_data_dim());
     float lowerBound = dist;
@@ -51,9 +63,9 @@ CandidatesHeap HNSW_BatchIterator::scanGraph() {
         candidate_set.pop();
         idType cur_node_id = curr_el_pair.second;
 
-        unsigned int *cur_node_links_header = hnsw_index.get_linklist_at_level(cur_node_id, 0);
-        unsigned short links_num = hnsw_index.getListCount(cur_node_links_header);
-        auto *node_links = (unsigned int *)(cur_node_links_header + 1);
+        uint *cur_node_links_header = hnsw_index.get_linklist_at_level(cur_node_id, 0);
+        ushort links_num = hnsw_index.getListCount(cur_node_links_header);
+        auto *node_links = (uint *)(cur_node_links_header + 1);
 #ifdef USE_SSE
         _mm_prefetch((char *)(visited_array + *(node_ll + 1)), _MM_HINT_T0);
         _mm_prefetch((char *)(visited_array + *(node_ll + 1) + 64), _MM_HINT_T0);
@@ -67,20 +79,19 @@ CandidatesHeap HNSW_BatchIterator::scanGraph() {
             _mm_prefetch((char *)(visited_array + *(node_links + j + 1)), _MM_HINT_T0);
             _mm_prefetch(getDataByInternalId(*(node_links + j + 1)), _MM_HINT_T0);
 #endif
-            if (visited_array[candidate_id] == cur_visited_tag ||
-            visited_array[candidate_id] == cur_returned_and_visited_tag) {
+            if (visited_array[candidate_id] == this->cur_visited_tag ||
+            visited_array[candidate_id] == this->cur_returned_visited_tag) {
                 continue;
             }
             if (!this->allow_marked_candidates && this->hasReturned(candidate_id)) {
                 continue;
             }
             if (hasReturned(candidate_id)) {
-                visited_array[candidate_id] = cur_returned_and_visited_tag;
+                visited_array[candidate_id] = this->cur_returned_visited_tag;
             } else {
-                visited_array[candidate_id] = cur_visited_tag;
+                visited_array[candidate_id] = this->cur_visited_tag;
             }
             char *candidate_data = hnsw_index.getDataByInternalId(candidate_id);
-
             float candidate_dist = dist_func(this->getQueryBlob(), (const void *)candidate_data,
                                              space->get_data_dim());
             if (top_candidates.size() < hnsw_index.getEf() || lowerBound > candidate_dist) {
@@ -90,11 +101,13 @@ CandidatesHeap HNSW_BatchIterator::scanGraph() {
 #endif
                 if (!this->hasReturned(candidate_id)) {
                     top_candidates.emplace(candidate_dist, candidate_id);
+                    this->markReturned(candidate_id);
                 }
 
                 if (top_candidates.size() > hnsw_index.getEf()) {
                     // set as entry point for next iterations the best node found but hasn't returned
                     this->entry_point = top_candidates.top().second;
+                    this->unmarkReturned(top_candidates.top().second);
                     top_candidates.pop();
                 }
 
@@ -103,59 +116,55 @@ CandidatesHeap HNSW_BatchIterator::scanGraph() {
             }
         }
     }
-#ifdef ENABLE_PARALLELIZATION
-    visited_list_pool_->returnVisitedListToPool(vl);
-#endif
-
-    return top_candidates;
+    return make_unique<CandidatesHeap>(top_candidates);
 }
 
 HNSW_BatchIterator::HNSW_BatchIterator(const void *query_vector, const HNSWIndex *hnsw_index,
                    std::shared_ptr<VecSimAllocator> allocator) : VecSimBatchIterator(query_vector, allocator),
                    // the search_id and the visited list is determined in the first iteration.
                    index(hnsw_index), allow_marked_candidates(false),
-                   depleted(false) {
-    entry_point = hnsw_index->getEntryPointId();
+                   depleted(false), results(nullptr) {
 
+    this->entry_point = hnsw_index->getEntryPointId();
     // Save the current state of the visited list, and derive tags in which we are going to use
     // from the current tag. We will use these "fresh" tags to mark returned results and visited nodes.
-    visited_list = this->index->hnsw.getVisitedList();
+    this->visited_list = this->index->hnsw.getVisitedList();
     // reset again to obtain 2 alternating tags for marking visited nodes in a specific iteration.
-    minimal_tag = visited_tag = this->visited_list->curTag;
+    this->tag_range_start = this->visited_list->curTag;
+    this->cur_visited_tag = this->tag_range_start+VISITED_ODD_OFFSET;
     this->visited_list->reset();
     // reset again twice to get another 2 alternating tags, to mark nodes that were returned and scanned
     // in a specific iteration.
     this->visited_list->reset();
-    visited_and_returned_tag = this->visited_list->curTag;
+    this->cur_returned_visited_tag = this->visited_list->curTag;
     this->visited_list->reset();
 }
 
 VecSimQueryResult_List HNSW_BatchIterator::getNextResults(size_t n_res, VecSimQueryResult_Order order) {
 
+    // In the first iteration, we search the graph from top bottom to find the initial entry point,
+    // and then we scan the graph to get results (layer 0).
     if (this->getResultsCount() == 0) {
         idType bottom_layer_ep = this->index->hnsw.searchBottomLayerEP(this->getQueryBlob());
         this->entry_point = bottom_layer_ep;
-        auto initial_results = make_unique<CandidatesHeap>(this->scanGraph());
-        results = *initial_results;
+        results = this->scanGraph();
     }
-
-    // First take results from the results set that we already found
     auto *batch_results = array_new<VecSimQueryResult>(n_res);
-    for (int i = 0; i < min(results.size(), n_res); i++) {
-        this->markReturned(results.top().second);
-        batch_results = array_append(batch_results, VecSimQueryResult{});
-        VecSimQueryResult_SetId(batch_results[i], results.top().second);
-        VecSimQueryResult_SetScore(batch_results[i], results.top().first);
-        results.pop();
+    while (array_len(batch_results) < n_res) {
+        for (int i = 0; i < min(results->size(), n_res-array_len(batch_results)); i++) {
+            batch_results = array_append(batch_results, VecSimQueryResult{});
+            VecSimQueryResult_SetId(batch_results[i], results->top().second);
+            VecSimQueryResult_SetScore(batch_results[i], results->top().first);
+            results->pop();
+        }
+        if (array_len(batch_results) == n_res || this->depleted) {
+            this->updateResultsCount(array_len(batch_results));
+            return batch_results;
+        }
+        // Otherwise, scan graph for more results, and save them.
+        results.reset(this->scanGraph().get());
     }
-    if (array_len(batch_results) == n_res) {
-        this->updateResultsCount(n_res);
-        return batch_results;
-    }
-
-    // Otherwise, scan graph for more results.
-    auto more_results = make_unique<CandidatesHeap>(this->scanGraph());
-    results = *more_results;
+    return batch_results;
 }
 
 bool HNSW_BatchIterator::isDepleted() {
--- src/VecSim/algorithms/hnsw/hnsw_batch_iterator.h
+++ src/VecSim/algorithms/hnsw/hnsw_batch_iterator.h
@@ -6,9 +6,7 @@
 typedef size_t labelType;
 typedef size_t idType;
 
-// use the maximum uint as the invalid id, since the search id is represented by unsigned short
-#define INVALID_SEARCH_ID std::numeric_limits<unsigned int>::max()
-
+typedef enum {VISITED_ODD_OFFSET, VISITED_EVEN_OFFSET, RETURNED_VISITED_ODD_OFFSET, RETURNED_VISITED_EVEN_OFFSET} TagOffset;
 using namespace std;
 
 struct CompareByFirst {
@@ -23,27 +21,26 @@ using CandidatesHeap = vecsim_stl::priority_queue<pair<float, labelType>>;
 class HNSW_BatchIterator : public VecSimBatchIterator {
 private:
     const HNSWIndex *index;
-    CandidatesHeap results; // results to return immediately in the next iteration.
+    unique_ptr<CandidatesHeap> results; // results to return immediately in the next iteration.
     idType entry_point; // internal id of the node to begin the scan from in the next iteration.
     bool allow_marked_candidates; // flag that indicates if we allow the search to visit in nodes that
                                   // where returned in previous iterations
     hnswlib::VisitedList *visited_list; // Pointer to the hnsw visitedList structure.
-    uint minimal_tag; // save the minimal tag which is used for nodes that were visited by this iterator.
-    uint visited_tag; // use to mark nodes that were scanned in this iteration.
-    uint visited_and_returned_tag; // use to mark nodes that were returned in previous iteration,
-                                   // and scanned in the current iteration
+    u_char tag_range_start; // save the minimal tag which is used to mark nodes that were visited and/or
+                            // returned by this iterator.
+    u_char cur_visited_tag; // used to mark nodes that were scanned in this iteration (that hasn't returned by the iterator).
+    u_char cur_returned_visited_tag; // use to mark nodes that were returned in previous iteration,
+                                     // and scanned in the current iteration
     bool depleted;
 
-    CandidatesHeap scanGraph();
-    bool hasReturned(idType node_id) const;
+    unique_ptr<CandidatesHeap> scanGraph();
+    inline bool hasReturned(idType node_id) const;
+    inline void markReturned (uint node_id);
+    inline void unmarkReturned (uint node_id);
 
 public:
     HNSW_BatchIterator(const void *query_vector, const HNSWIndex *index,
                      std::shared_ptr<VecSimAllocator> allocator);
-
-    inline void markReturned (uint node_id) {
-        this->visited_list->visitedElements[node_id] = this->visited_and_returned_tag;
-    }
 
     VecSimQueryResult_List getNextResults(size_t n_res, VecSimQueryResult_Order order) override;
 
