22c089d48c9681f9b56395aa14a6c8babc18f134
alon <alonreshef24@gmail.com>
2021-12-28 13:31:27 +0200

    Change ushort and uint to unsigned

--- src/VecSim/algorithms/hnsw/hnsw_batch_iterator.h
+++ src/VecSim/algorithms/hnsw/hnsw_batch_iterator.h
@@ -4,7 +4,7 @@
 #include "hnswlib_c.h"
 
 typedef size_t labelType;
-typedef uint idType;
+typedef unsigned int idType;
 
 using namespace std;
 
@@ -17,7 +17,7 @@ private:
     std::unique_ptr<SpaceInterface<float>> space;
     hnswlib::HierarchicalNSW<float> *hnsw_index;
     hnswlib::VisitedNodesHandler *visited_list; // Pointer to the hnsw visitedList structure.
-    ushort visited_tag;                         // Used to mark nodes that were scanned.
+    unsigned short visited_tag;                 // Used to mark nodes that were scanned.
     idType entry_point;                         // Internal id of the node to begin the scan from.
     bool depleted;
 
