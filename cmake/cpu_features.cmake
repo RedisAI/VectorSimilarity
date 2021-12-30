119b05f7549aae03b05638223312bf1cf0e8a742
DvirDukhan <dvir@redis.com>
2021-12-30 18:16:06 +0200

    gtest required for cpu_features

--- cmake/cpu_features.cmake
+++
@@ -1,8 +0,0 @@
-project(cpu_features)
-
-include(FetchContent)
-FetchContent_Declare(
-	cpu_features
-	URL https://github.com/google/cpu_features/archive/refs/tags/v0.6.0.zip
-)
-FetchContent_MakeAvailable(cpu_features)
