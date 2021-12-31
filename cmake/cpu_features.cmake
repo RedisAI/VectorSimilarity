2035ac9c9f976ddc802555fb0368529d4551ef26
DvirDukhan <dvir@redis.com>
2021-12-31 09:19:02 +0000

    fixed PR comment

---
+++ cmake/cpu_features.cmake
@@ -0,0 +1,8 @@
+include(FetchContent)
+option(BUILD_TESTING "" OFF)
+option(CMAKE_POSITION_INDEPENDENT_CODE ON)
+FetchContent_Declare(
+	cpu_features
+	GIT_REPOSITORY  https://github.com/google/cpu_features.git
+)
+FetchContent_MakeAvailable(cpu_features)
