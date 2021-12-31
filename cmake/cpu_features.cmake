ab28edaa9d4a7047401d020febbe70893805cf06
DvirDukhan <dvir@redis.com>
2021-12-31 09:24:54 +0000

    fixed cpu_features.cmake settings for sanitizer

--- cmake/cpu_features.cmake
+++ cmake/cpu_features.cmake
@@ -1,6 +1,6 @@
 include(FetchContent)
 option(BUILD_TESTING "" OFF)
-option(CMAKE_POSITION_INDEPENDENT_CODE ON)
+option(CMAKE_POSITION_INDEPENDENT_CODE "" ON)
 FetchContent_Declare(
 	cpu_features
 	GIT_REPOSITORY  https://github.com/google/cpu_features.git
