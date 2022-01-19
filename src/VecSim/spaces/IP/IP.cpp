0299c5a3bcb340176c829f35c24511b83c7078c9
GuyAv46 <guy.avimor@gmail.com>
2022-01-19 17:03:36 +0200

    bug fix in IP optimization function, that caused rounding results

--- src/VecSim/spaces/IP/IP.cpp
+++ src/VecSim/spaces/IP/IP.cpp
@@ -3,14 +3,15 @@
 
 #include <stdlib.h>
 
-float InnerProduct(const void *pVect1, const void *pVect2, const void *qty_ptr) {
+float InnerProduct_impl(const void *pVect1, const void *pVect2, const void *qty_ptr) {
     size_t qty = *((size_t *)qty_ptr);
-    double res = 0;
-    double e1, e2;
+    float res = 0;
     for (unsigned i = 0; i < qty; i++) {
-        e1 = ((float *)pVect1)[i];
-        e2 = ((float *)pVect2)[i];
-        res += e1 * e2;
+        res += ((float *)pVect1)[i] * ((float *)pVect2)[i];
     }
-    return 1.0f - res;
+    return res;
+}
+
+float InnerProduct(const void *pVect1, const void *pVect2, const void *qty_ptr) {
+    return 1.0f - InnerProduct_impl(pVect1, pVect2, qty_ptr);
 }
--- src/VecSim/spaces/IP/IP.h
+++ src/VecSim/spaces/IP/IP.h
@@ -1,3 +1,5 @@
 #pragma once
 
 float InnerProduct(const void *pVect1, const void *pVect2, const void *qty_ptr);
+
+float InnerProduct_impl(const void *pVect1, const void *pVect2, const void *qty_ptr);
