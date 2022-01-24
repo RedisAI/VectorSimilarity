64eab1444a03f7a25fcfd5a0c44639f5612cbece
DvirDukhan <dvir@redislabs.com>
2022-01-24 20:46:17 +0200

    Update cpu_features.cmake

--- cmake/cpu_features.cmake
+++ cmake/cpu_features.cmake
@@ -4,5 +4,6 @@ option(CMAKE_POSITION_INDEPENDENT_CODE "" ON)
 FetchContent_Declare(
 	cpu_features
 	GIT_REPOSITORY  https://github.com/google/cpu_features.git
+	GIT_TAG origin/main
 )
 FetchContent_MakeAvailable(cpu_features)
