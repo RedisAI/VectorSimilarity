eebc87c979558af88c9ee798bad7aa2364a81dbf
rafie <rafi@redislabs.com>
2022-01-26 12:53:30 +0200

    fixes 3

--- cmake/common.cmake
+++ cmake/common.cmake
@@ -19,11 +19,6 @@ message("# OS=${OS}")
 message("# OSNICK=${OSNICK}")
 message("# ARCH=${ARCH}")
 
-# if (NOT DEFINED VECSIM_MARCH AND ARCH STREQUAL "x64")
-# 	set(VECSIM_MARCH "x86-64-v4")
-# endif()
-# message("# VECSIM_MARCH: " ${VECSIM_MARCH})
-
 if (USE_COVERAGE)
     if (NOT CMAKE_BUILD_TYPE STREQUAL "DEBUG")
         message(FATAL_ERROR "Build type must be DEBUG for coverage")
