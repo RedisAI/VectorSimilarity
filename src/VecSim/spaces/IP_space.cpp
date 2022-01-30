8ea467cce23e8aecce23f6dfe710c2b4db909738
DvirDukhan <dvir@redislabs.com>
2022-01-30 09:51:03 +0200

    check avx512f avx sse compiler flags (#128)
    * built with gcc9 on focal
    
    * all os CI
    
    * tiny build steps

--- src/VecSim/spaces/IP_space.cpp
+++ src/VecSim/spaces/IP_space.cpp
@@ -1,9 +1,6 @@
 #include "IP_space.h"
 #include "space_aux.h"
 #include "VecSim/spaces/IP/IP.h"
-#include "VecSim/spaces/IP/IP_SSE.h"
-#include "VecSim/spaces/IP/IP_AVX.h"
-#include "VecSim/spaces/IP/IP_AVX512.h"
 
 InnerProductSpace::InnerProductSpace(size_t dim, std::shared_ptr<VecSimAllocator> allocator)
     : SpaceInterface(allocator) {
@@ -11,12 +8,17 @@ InnerProductSpace::InnerProductSpace(size_t dim, std::shared_ptr<VecSimAllocator
 #if defined(__x86_64__)
     Arch_Optimization arch_opt = getArchitectureOptimization();
     if (arch_opt == ARCH_OPT_AVX512) {
+#include "VecSim/spaces/IP/IP_AVX512.h"
+#ifdef __AVX512F__
         if (dim % 16 == 0) {
             fstdistfunc_ = InnerProductSIMD16Ext_AVX512;
         } else {
             fstdistfunc_ = InnerProductSIMD16ExtResiduals_AVX512;
         }
+#endif
     } else if (arch_opt == ARCH_OPT_AVX) {
+#ifdef __AVX__
+#include "VecSim/spaces/IP/IP_AVX.h"
         if (dim % 16 == 0) {
             fstdistfunc_ = InnerProductSIMD16Ext_AVX;
         } else if (dim % 4 == 0) {
@@ -26,7 +28,10 @@ InnerProductSpace::InnerProductSpace(size_t dim, std::shared_ptr<VecSimAllocator
         } else if (dim > 4) {
             fstdistfunc_ = InnerProductSIMD4ExtResiduals_AVX;
         }
+#endif
     } else if (arch_opt == ARCH_OPT_SSE) {
+#ifdef __SSE__
+#include "VecSim/spaces/IP/IP_SSE.h"
         if (dim % 16 == 0) {
             fstdistfunc_ = InnerProductSIMD16Ext_SSE;
         } else if (dim % 4 == 0) {
@@ -36,6 +41,7 @@ InnerProductSpace::InnerProductSpace(size_t dim, std::shared_ptr<VecSimAllocator
         } else if (dim > 4) {
             fstdistfunc_ = InnerProductSIMD4ExtResiduals_SSE;
         }
+#endif
     }
 #endif // __x86_64__
     dim_ = dim;
