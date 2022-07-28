54cc3fabcb77a0466a43cb11964cbbdf9c11d87e
GuyAv46 <47632673+GuyAv46@users.noreply.github.com>
2022-07-28 22:58:36 +0300

    Space Optimization Fix (#175)
    * improving functions
    
    * improving logic of optimization choosing
    
    * first metrics benchmark
    
    * improved benchmark
    
    * added tests for all new functions
    
    * improved benchmark and added comments
    
    * added logic explanation
    
    * fixing alignment
    
    * added bm_spaces to "make benchmark" run
    
    * added arch check in compile time
    
    * refactor benchmark file
    
    * comment fix

--- src/VecSim/spaces/IP/IP_AVX512.cpp
+++ src/VecSim/spaces/IP/IP_AVX512.cpp
@@ -7,13 +7,12 @@
 
 float InnerProductSIMD16Ext_AVX512_impl(const void *pVect1v, const void *pVect2v,
                                         const void *qty_ptr) {
-    float PORTABLE_ALIGN64 TmpRes[16];
     float *pVect1 = (float *)pVect1v;
     float *pVect2 = (float *)pVect2v;
     size_t qty = *((size_t *)qty_ptr);
-    size_t qty16 = qty / 16;
+    size_t qty16 = qty >> 4 << 4;
 
-    const float *pEnd1 = pVect1 + 16 * qty16;
+    const float *pEnd1 = pVect1 + qty16;
 
     __m512 sum512 = _mm512_set1_ps(0);
 
@@ -24,18 +23,61 @@ float InnerProductSIMD16Ext_AVX512_impl(const void *pVect1v, const void *pVect2v
         __m512 v2 = _mm512_loadu_ps(pVect2);
         pVect2 += 16;
         sum512 = _mm512_add_ps(sum512, _mm512_mul_ps(v1, v2));
+        // sum512 = _mm512_fmadd_ps(v1, v2, sum512);
     }
 
-    _mm512_store_ps(TmpRes, sum512);
-    float sum = TmpRes[0] + TmpRes[1] + TmpRes[2] + TmpRes[3] + TmpRes[4] + TmpRes[5] + TmpRes[6] +
-                TmpRes[7] + TmpRes[8] + TmpRes[9] + TmpRes[10] + TmpRes[11] + TmpRes[12] +
-                TmpRes[13] + TmpRes[14] + TmpRes[15];
-
-    return sum;
+    return _mm512_reduce_add_ps(sum512);
 }
 
 float InnerProductSIMD16Ext_AVX512(const void *pVect1, const void *pVect2, const void *qty_ptr) {
     return 1.0f - InnerProductSIMD16Ext_AVX512_impl(pVect1, pVect2, qty_ptr);
+}
+
+float InnerProductSIMD4Ext_AVX512_impl(const void *pVect1v, const void *pVect2v,
+                                       const void *qty_ptr) {
+    float PORTABLE_ALIGN16 TmpRes[4];
+    float *pVect1 = (float *)pVect1v;
+    float *pVect2 = (float *)pVect2v;
+    size_t qty = *((size_t *)qty_ptr);
+
+    size_t qty16 = qty >> 4 << 4;
+    size_t qty4 = qty >> 2 << 2;
+
+    const float *pEnd1 = pVect1 + qty16;
+    const float *pEnd2 = pVect1 + qty4;
+
+    __m512 sum512 = _mm512_set1_ps(0);
+
+    while (pVect1 < pEnd1) {
+
+        __m512 v1 = _mm512_loadu_ps(pVect1);
+        pVect1 += 16;
+        __m512 v2 = _mm512_loadu_ps(pVect2);
+        pVect2 += 16;
+        sum512 = _mm512_add_ps(sum512, _mm512_mul_ps(v1, v2));
+        // sum512 = _mm512_fmadd_ps(v1, v2, sum512);
+    }
+
+    __m128 v1, v2;
+    __m128 sum_prod = _mm512_extractf32x4_ps(sum512, 0) + _mm512_extractf32x4_ps(sum512, 1) +
+                      _mm512_extractf32x4_ps(sum512, 2) + _mm512_extractf32x4_ps(sum512, 3);
+
+    while (pVect1 < pEnd2) {
+        v1 = _mm_loadu_ps(pVect1);
+        pVect1 += 4;
+        v2 = _mm_loadu_ps(pVect2);
+        pVect2 += 4;
+        sum_prod = _mm_add_ps(sum_prod, _mm_mul_ps(v1, v2));
+    }
+
+    _mm_store_ps(TmpRes, sum_prod);
+    float sum = TmpRes[0] + TmpRes[1] + TmpRes[2] + TmpRes[3];
+
+    return sum;
+}
+
+float InnerProductSIMD4Ext_AVX512(const void *pVect1, const void *pVect2, const void *qty_ptr) {
+    return 1.0f - InnerProductSIMD4Ext_AVX512_impl(pVect1, pVect2, qty_ptr);
 }
 
 float InnerProductSIMD16ExtResiduals_AVX512(const void *pVect1v, const void *pVect2v,
@@ -47,6 +89,19 @@ float InnerProductSIMD16ExtResiduals_AVX512(const void *pVect1v, const void *pVe
     float *pVect2 = (float *)pVect2v + qty16;
 
     size_t qty_left = qty - qty16;
+    float res_tail = InnerProduct_impl(pVect1, pVect2, &qty_left);
+    return 1.0f - (res + res_tail);
+}
+
+float InnerProductSIMD4ExtResiduals_AVX512(const void *pVect1v, const void *pVect2v,
+                                           const void *qty_ptr) {
+    size_t qty = *((size_t *)qty_ptr);
+    size_t qty4 = qty >> 2 << 2;
+    float res = InnerProductSIMD4Ext_AVX512_impl(pVect1v, pVect2v, &qty4);
+    float *pVect1 = (float *)pVect1v + qty4;
+    float *pVect2 = (float *)pVect2v + qty4;
+
+    size_t qty_left = qty - qty4;
     float res_tail = InnerProduct_impl(pVect1, pVect2, &qty_left);
     return 1.0f - (res + res_tail);
 }
