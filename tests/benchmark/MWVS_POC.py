f4fdde3d3da1eab26a9af29b7778af8c1e320f8a
alon <alonreshef24@gmail.com>
2022-08-11 09:49:06 +0300

    POC (cont.) WIP

--- tests/benchmark/MWVS_POC.py
+++ tests/benchmark/MWVS_POC.py
@@ -19,8 +19,12 @@ def create_indexes(params, data_set, index_file_name):
     bfparams.type = VecSimType_FLOAT32
     bfparams.metric = VecSimMetric_Cosine
     flat_index = BFIndex(bfparams)
-    for i, vector in enumerate(data_set):
+    for i, vector in enumerate(data_set[:1000000]):
         flat_index.add_vector(vector, i)
+
+    index_size = hnsw_index.index_size()
+    for i in range(1000000, index_size):
+        hnsw_index.delete_vector(i)
 
     print(f"loaded HNSW index from file {index_file_name}, index of size {hnsw_index.index_size()}, "
           f"and created a corresponding flat index of size {flat_index.index_size()}")
@@ -59,6 +63,7 @@ def main():
     # Create 3 HNSW indexes and tests_sets:
     test_sets = {}
     data_sets = ["dbpedia-768", "glove-50", "glove-200"]
+    dataset_size = 1000000
 
     # DBPedia #
     hnswparams = HNSWParams()
@@ -119,13 +124,39 @@ def main():
         BF_res = get_BF_results(dbpedia_index_flat, glove_50_index_flat, glove_200_index_flat,
                                                    q0, q1, q2, w0, w1, w2, k)
 
-        print(BF_res)
+        #print(BF_res)
         res_dbpedia_ids, res_dbpedia_scores = dbpedia_index.knn_query(q0, k)
-        res_glove_50_ids, res_glove_50_scores = dbpedia_index.knn_query(q1, k)
-        res_glove_200_ids, res_glove_50_scores = dbpedia_index.knn_query(q2, k)
-        total_res = reduce(np.union1d, (res_dbpedia_ids[0], res_glove_50_ids[0], res_glove_200_ids[0]))
+        res_glove_50_ids, res_glove_50_scores = glove_50_index.knn_query(q1, k)
+        res_glove_200_ids, res_glove_50_scores = glove_200_index.knn_query(q2, k)
+        total_res = reduce(np.union1d, (res_dbpedia_ids[0], res_glove_50_ids[0], res_glove_200_ids[0])).tolist()
 
-        # measure recall by this point
+        combined_scores = []
+        for i, res in enumerate(total_res):
+            combined_score = w0 * dbpedia_index_flat.get_distance_from(q0, total_res[i]) + \
+                     w1 * glove_50_index_flat.get_distance_from(q1, total_res[i]) + \
+                     w2 * glove_200_index_flat.get_distance_from(q2, total_res[i])
+            combined_scores.append(combined_score)
+
+        T = sorted(combined_scores)[k-1]
+        print (f"Threshold is {T}")
+
+        # measure results that were found by this point
+        actual_res_ids = [res[1] for res in BF_res]
+        correct = 0
+        for res in total_res:
+            for actual_res in actual_res_ids:
+                if res == actual_res:
+                    correct += 1
+                    break
+
+        print(f"Current results found is {correct} from {len(total_res)} results obtained")
+
+        res_dbpedia_ids, res_dbpedia_scores = dbpedia_index.range_query(q0, T)
+        res_glove_50_ids, res_glove_50_scores = glove_50_index.range_query(q1, T)
+        res_glove_200_ids, res_glove_200_scores = glove_200_index.range_query(q2, T)
+        total_res = reduce(np.union1d, (res_dbpedia_ids[0], res_glove_50_ids[0], res_glove_200_ids[0])).tolist()
+        print(f"total res size is {len(total_res)}")
+
 
 
 if __name__ == '__main__':
