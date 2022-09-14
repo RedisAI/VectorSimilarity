8d1541d16cf6c6a3e5723306daa3a8d25559ca09
meiravgri <meirav.grimberg@redis.com>
2022-09-14 06:55:43 +0300

    added 512 iteration to dim %4 == 0

--- src/VecSim/spaces/L2/L2_SSE_FP32.cpp
+++ src/VecSim/spaces/L2/L2_SSE_FP32.cpp
@@ -65,13 +65,48 @@ float FP32_L2SqrSIMD4Ext_SSE(const void *pVect1v, const void *pVect2v, size_t qt
     float *pVect1 = (float *)pVect1v;
     float *pVect2 = (float *)pVect2v;
 
-    const float *pEnd1 = pVect1 + qty;
+    // Calc how many floats we can calc using 512 bits iterations.
+    size_t qty16 = qty >> 4 << 4;
+    const float *pEnd1 = pVect1 + qty16;
 
     __m128 diff, v1, v2;
     __m128 sum = _mm_set1_ps(0);
 
-    // In each iteration we calculate 4 floats = 128 bits.
+    // In each iteration we calculate 16 floats = 512 bits.
     while (pVect1 < pEnd1) {
+        v1 = _mm_loadu_ps(pVect1);
+        pVect1 += 4;
+        v2 = _mm_loadu_ps(pVect2);
+        pVect2 += 4;
+        diff = _mm_sub_ps(v1, v2);
+        sum = _mm_add_ps(sum, _mm_mul_ps(diff, diff));
+
+        v1 = _mm_loadu_ps(pVect1);
+        pVect1 += 4;
+        v2 = _mm_loadu_ps(pVect2);
+        pVect2 += 4;
+        diff = _mm_sub_ps(v1, v2);
+        sum = _mm_add_ps(sum, _mm_mul_ps(diff, diff));
+
+        v1 = _mm_loadu_ps(pVect1);
+        pVect1 += 4;
+        v2 = _mm_loadu_ps(pVect2);
+        pVect2 += 4;
+        diff = _mm_sub_ps(v1, v2);
+        sum = _mm_add_ps(sum, _mm_mul_ps(diff, diff));
+
+        v1 = _mm_loadu_ps(pVect1);
+        pVect1 += 4;
+        v2 = _mm_loadu_ps(pVect2);
+        pVect2 += 4;
+        diff = _mm_sub_ps(v1, v2);
+        sum = _mm_add_ps(sum, _mm_mul_ps(diff, diff));
+    }
+
+    const float *pEnd2 = pVect1 + qty;
+
+    // In each iteration we calculate 4 floats = 128 bits.
+    while (pVect1 < pEnd2) {
         v1 = _mm_loadu_ps(pVect1);
         pVect1 += 4;
         v2 = _mm_loadu_ps(pVect2);
