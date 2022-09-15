4ca58e1b17e87a5488ca9004d69f047e9d927571
meiravgri <meirav.grimberg@redis.com>
2022-09-15 10:38:42 +0300

    added 512 bits iteration to dim %2 ==0

--- src/VecSim/spaces/L2/L2_SSE_FP64.cpp
+++ src/VecSim/spaces/L2/L2_SSE_FP64.cpp
@@ -66,13 +66,38 @@ double FP64_L2SqrSIMD2Ext_SSE(const void *pVect1v, const void *pVect2v, size_t q
     double *pVect1 = (double *)pVect1v;
     double *pVect2 = (double *)pVect2v;
 
-    const double *pEnd1 = pVect1 + qty;
+    // Calc how many doubles we can calc using 512 bits iterations.
+    size_t qty8 = qty >> 3 << 3;
+    const double *pEnd1 = pVect1 + qty8;
+
+    const double *pEnd2 = pVect1 + qty;
 
     __m128d diff, v1, v2;
     __m128d sum = _mm_set1_pd(0);
 
-    // In each iteration we calculate 2 doubles = 128 bits.
+    // In each iteration we calculate 8 doubles = 512 bits in total.
     while (pVect1 < pEnd1) {
+        v1 = _mm_loadu_pd(pVect1);
+        pVect1 += 2;
+        v2 = _mm_loadu_pd(pVect2);
+        pVect2 += 2;
+        diff = _mm_sub_pd(v1, v2);
+        sum = _mm_add_pd(sum, _mm_mul_pd(diff, diff));
+
+        v1 = _mm_loadu_pd(pVect1);
+        pVect1 += 2;
+        v2 = _mm_loadu_pd(pVect2);
+        pVect2 += 2;
+        diff = _mm_sub_pd(v1, v2);
+        sum = _mm_add_pd(sum, _mm_mul_pd(diff, diff));
+
+        v1 = _mm_loadu_pd(pVect1);
+        pVect1 += 2;
+        v2 = _mm_loadu_pd(pVect2);
+        pVect2 += 2;
+        diff = _mm_sub_pd(v1, v2);
+        sum = _mm_add_pd(sum, _mm_mul_pd(diff, diff));
+
         v1 = _mm_loadu_pd(pVect1);
         pVect1 += 2;
         v2 = _mm_loadu_pd(pVect2);
@@ -81,6 +106,17 @@ double FP64_L2SqrSIMD2Ext_SSE(const void *pVect1v, const void *pVect2v, size_t q
         sum = _mm_add_pd(sum, _mm_mul_pd(diff, diff));
     }
 
+    // In each iteration we calculate 2 doubles = 128 bits.
+    while (pVect1 < pEnd2) {
+        v1 = _mm_loadu_pd(pVect1);
+        pVect1 += 2;
+        v2 = _mm_loadu_pd(pVect2);
+        pVect2 += 2;
+        diff = _mm_sub_pd(v1, v2);
+        sum = _mm_add_pd(sum, _mm_mul_pd(diff, diff));
+    }
+
+    // TmpRes must be 16 bytes aligned
     double PORTABLE_ALIGN16 TmpRes[2];
     _mm_store_pd(TmpRes, sum);
     return TmpRes[0] + TmpRes[1];
