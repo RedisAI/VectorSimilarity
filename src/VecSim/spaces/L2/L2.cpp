f4e51d0441d5d6946d436ab58d2347970f576cc8
meiravgri <109056284+meiravgri@users.noreply.github.com>
2022-09-23 08:00:43 +0300

    Fp64 dist function (#250)
    * Distance calculation functions for doubles - no optimizations (#231)
    
    * Distance calculation functions for doubles - L2 SSE (#239)

--- src/VecSim/spaces/L2/L2.cpp
+++ src/VecSim/spaces/L2/L2.cpp
@@ -1,14 +1,24 @@
 #include "L2.h"
 
 float FP32_L2Sqr(const void *pVect1v, const void *pVect2v, size_t qty) {
-    float *pVect1 = (float *)pVect1v;
-    float *pVect2 = (float *)pVect2v;
+    float *vec1 = (float *)pVect1v;
+    float *vec2 = (float *)pVect2v;
 
     float res = 0;
     for (size_t i = 0; i < qty; i++) {
-        float t = *pVect1 - *pVect2;
-        pVect1++;
-        pVect2++;
+        float t = vec1[i] - vec2[i];
+        res += t * t;
+    }
+    return res;
+}
+
+double FP64_L2Sqr(const void *pVect1v, const void *pVect2v, size_t qty) {
+    double *vec1 = (double *)pVect1v;
+    double *vec2 = (double *)pVect2v;
+
+    double res = 0;
+    for (size_t i = 0; i < qty; i++) {
+        double t = vec1[i] - vec2[i];
         res += t * t;
     }
     return res;
