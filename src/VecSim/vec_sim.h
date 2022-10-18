004fc9112570d80e39ca9da8b0c52c0c058307a2
DvirDukhan <dvir@redis.com>
2022-10-18 11:13:29 +0300

    Epsilon runtime parameter MOD-4165 (#257)

--- src/VecSim/vec_sim.h
+++ src/VecSim/vec_sim.h
@@ -95,11 +95,12 @@ size_t VecSimIndex_IndexSize(VecSimIndex *index);
  * @param rparams array of raw params to resolve.
  * @param paramNum number of params in rparams (or number of parames in rparams to resolve).
  * @param qparams pointer to VecSimQueryParams struct to set.
- * @param hybrid indicates if query is hybrid or "standard" VSS query.
+ * @param query_type indicates if query is hybrid, range or "standard" VSS query.
  * @return VecSim_OK if the resolve was successful, VecSimResolveCode error code if not.
  */
 VecSimResolveCode VecSimIndex_ResolveParams(VecSimIndex *index, VecSimRawParam *rparams,
-                                            int paramNum, VecSimQueryParams *qparams, bool hybrid);
+                                            int paramNum, VecSimQueryParams *qparams,
+                                            VecsimQueryType query_type);
 
 /**
  * @brief Search for the k closest vectors to a given vector in the index. The results can be
