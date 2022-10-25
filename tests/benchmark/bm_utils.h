aeb13dba5e498790c722a3a081a265fcdc18b5a0
meiravgri <meirav.grimberg@redis.com>
2022-10-25 16:11:55 +0300

    removed basics from makefile, mnoved getdatabyinternalid of BF ro public if BUILD_TESTS, added all needed tests to bm basics

--- tests/benchmark/bm_utils.h
+++ tests/benchmark/bm_utils.h
@@ -26,7 +26,6 @@ protected:
     static size_t M;
     static size_t EF_C;
     static size_t block_size;
-    
 
     static const char *test_vectors_file;
     static size_t n_queries;
@@ -65,7 +64,6 @@ private:
     }
 };
 
-
 template <bool is_multi>
 BM_VecSimBasics<is_multi>::BM_VecSimBasics(VecSimType type) {
     if (ref_count == 0) {
@@ -80,19 +78,19 @@ void BM_VecSimBasics<is_multi>::Initialize(VecSimType type) {
 
     // Initialize and load HNSW index for DBPedia data set.
     HNSWParams hnsw_params = {.type = type,
-                         .dim = BM_VecSimBasics::dim,
-                         .metric = VecSimMetric_Cosine,
-                         .multi = is_multi,
-                         .initialCapacity = BM_VecSimBasics::n_vectors,
-                         .blockSize = BM_VecSimBasics::block_size,
-                         .M = BM_VecSimBasics::M,
-                         .efConstruction = BM_VecSimBasics::EF_C};
+                              .dim = BM_VecSimBasics::dim,
+                              .metric = VecSimMetric_Cosine,
+                              .multi = is_multi,
+                              .initialCapacity = BM_VecSimBasics::n_vectors,
+                              .blockSize = BM_VecSimBasics::block_size,
+                              .M = BM_VecSimBasics::M,
+                              .efConstruction = BM_VecSimBasics::EF_C};
 
-    BFParams bf_params ={.type = type,
-                        .dim = BM_VecSimBasics::dim,
-                        .metric = VecSimMetric_Cosine,
-                        .initialCapacity = BM_VecSimBasics::n_vectors,
-                        .blockSize = BM_VecSimBasics::block_size};
+    BFParams bf_params = {.type = type,
+                          .dim = BM_VecSimBasics::dim,
+                          .metric = VecSimMetric_Cosine,
+                          .initialCapacity = BM_VecSimBasics::n_vectors,
+                          .blockSize = BM_VecSimBasics::block_size};
 
     InitializeIndicesVector(CreateNewIndex(bf_params), CreateNewIndex(hnsw_params));
 
@@ -109,7 +107,6 @@ void BM_VecSimBasics<is_multi>::Initialize(VecSimType type) {
     load_test_vectors();
 }
 
-
 template <bool is_multi>
 void BM_VecSimBasics<is_multi>::load_test_vectors() {
     auto location = std::string(std::string(getenv("ROOT")));
@@ -122,10 +119,41 @@ void BM_VecSimBasics<is_multi>::load_test_vectors() {
     }
     input.seekg(0, std::ifstream::beg);
     InsertToQueries(input);
-
-   
 }
 
+template <bool is_multi>
+template <typename data_t>
+void BM_VecSimBasics<is_multi>::RunTopK_HNSW(benchmark::State &st, size_t ef, size_t iter, size_t k,
+                                             size_t &correct, VecSimIndex *hnsw_index_,
+                                             VecSimIndex *bf_index_,
+                                             const std::vector<std::vector<data_t>> &queries) {
+    auto query_params = VecSimQueryParams{.hnswRuntimeParams = HNSWRuntimeParams{.efRuntime = ef}};
+    auto hnsw_results = VecSimIndex_TopKQuery(hnsw_index_, queries[iter % n_queries].data(), k,
+                                              &query_params, BY_SCORE);
+    st.PauseTiming();
+
+    // Measure recall:
+    auto bf_results =
+        VecSimIndex_TopKQuery(bf_index_, queries[iter % n_queries].data(), k, nullptr, BY_SCORE);
+    auto hnsw_it = VecSimQueryResult_List_GetIterator(hnsw_results);
+    while (VecSimQueryResult_IteratorHasNext(hnsw_it)) {
+        auto hnsw_res_item = VecSimQueryResult_IteratorNext(hnsw_it);
+        auto bf_it = VecSimQueryResult_List_GetIterator(bf_results);
+        while (VecSimQueryResult_IteratorHasNext(bf_it)) {
+            auto bf_res_item = VecSimQueryResult_IteratorNext(bf_it);
+            if (VecSimQueryResult_GetId(hnsw_res_item) == VecSimQueryResult_GetId(bf_res_item)) {
+                correct++;
+                break;
+            }
+        }
+        VecSimQueryResult_IteratorFree(bf_it);
+    }
+    VecSimQueryResult_IteratorFree(hnsw_it);
+
+    VecSimQueryResult_Free(bf_results);
+    VecSimQueryResult_Free(hnsw_results);
+    st.ResumeTiming();
+}
 /*
  *  Populate the given queries vector with the serialized raw vectors data in
  *  the file which is located in the given path.
