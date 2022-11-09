19416b478844423533987867dd9a82a754fc9db4
DvirDukhan <dvir@redis.com>
2022-11-09 23:17:50 +0200

    fixed rocky

--- .install/rocky_linux_8.6.sh
+++ .install/rocky_linux_8.6.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-yum install -y gcc-toolset-9-gcc gcc-toolset-9-gcc-c++ make valgrind wget git cmake
-source /opt/rh/gcc-toolset-9/enable
+yum install -y gcc-toolset-10-gcc gcc-toolset-10-gcc-c++ make valgrind wget git cmake
+source /opt/rh/gcc-toolset-10/enable
