d2d2e477bd59e0d755b12bd1a9380e4d2184bb12
GuyAv46 <guy.avimor@gmail.com>
2022-11-16 12:59:45 +0200

    license

--- src/VecSim/algorithms/ngt/ngt_factory.cpp
+++ src/VecSim/algorithms/ngt/ngt_factory.cpp
@@ -1,3 +1,9 @@
+/*
+ *Copyright Redis Ltd. 2021 - present
+ *Licensed under your choice of the Redis Source Available License 2.0 (RSALv2) or
+ *the Server Side Public License v1 (SSPLv1).
+ */
+
 #include "VecSim/algorithms/ngt/ngt_single.h"
 #include "VecSim/algorithms/ngt/ngt_multi.h"
 #include "VecSim/algorithms/ngt/ngt_factory.h"
