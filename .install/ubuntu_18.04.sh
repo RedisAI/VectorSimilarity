712fc181346fdc901444773193bd7f21bb0f4ead
DvirDukhan <dvir@redis.com>
2022-11-16 15:03:14 +0200

    fixed old ubuntus

--- .install/ubuntu_18.04.sh
+++ .install/ubuntu_18.04.sh
@@ -4,7 +4,7 @@ apt-get update -qq
 apt-get upgrade -yqq
 apt-get dist-upgrade -yqq
 apt install -yqq software-properties-common
-add-apt-repository ppa:ubuntu-toolchain-r/test -yqq
+add-apt-repository ppa:ubuntu-toolchain-r/test -y
 apt update
 apt-get install -yqq git wget make valgrind gcc-9 g++-9
 update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-9 60 --slave /usr/bin/g++ g++ /usr/bin/g++-9 
