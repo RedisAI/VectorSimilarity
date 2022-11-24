f80b9e56b2c85bfbd3224455f5ddb7db1c8c2753
GuyAv46 <guy.avimor@gmail.com>
2022-11-24 13:09:33 +0200

    cleanup

--- .install/ubuntu_22.04.sh
+++ .install/ubuntu_22.04.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
 apt-get update -qq
-apt-get install -yqq git wget gcc-10 g++-10 make valgrind
+apt-get install -yqq git wget build-essential valgrind
 source linux_install_cmake.sh
