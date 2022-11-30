7a9023d4a1d5c812a0dae1f415f3f65de57bbb57
GuyAv46 <guy.avimor@gmail.com>
2022-11-30 09:19:50 +0200

    added git to debian

--- .install/ubuntu_20.04.sh
+++ .install/ubuntu_20.04.sh
@@ -2,5 +2,5 @@
 set -e
 export DEBIAN_FRONTEND=noninteractive
 apt-get update -qq
-apt-get install -yqq wget make clang-format gcc valgrind python3-pip lcov
+apt-get install -yqq wget make clang-format gcc valgrind python3-pip lcov git
 source linux_install_cmake.sh
