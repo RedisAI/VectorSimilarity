80fb5bbcbc58f7b63b4d6a9edb18abb58dd3cc5a
DvirDukhan <dvir@redis.com>
2022-11-30 13:51:47 -0800

    added lcov to jammy

--- .install/ubuntu_22.04.sh
+++ .install/ubuntu_22.04.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
 apt-get update -qq
-apt-get install -yqq git wget build-essential valgrind
+apt-get install -yqq git wget build-essential valgrind lcov
 source linux_install_cmake.sh
