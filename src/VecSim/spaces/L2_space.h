107dd55e56e9af7b1b09d160c6d64506b28d28f2
DvirDukhan <dvir@redis.com>
2022-12-06 16:29:20 +0200

    GH actions CI (#283)

--- src/VecSim/spaces/L2_space.h
+++ src/VecSim/spaces/L2_space.h
@@ -8,7 +8,7 @@
 #include "VecSim/spaces/spaces.h"
 
 namespace spaces {
-dist_func_t<float> L2_FP32_GetDistFunc(size_t dim);
-dist_func_t<double> L2_FP64_GetDistFunc(size_t dim);
+dist_func_t<float> L2_FP32_GetDistFunc(size_t dim, const Arch_Optimization arch_opt);
+dist_func_t<double> L2_FP64_GetDistFunc(size_t dim, const Arch_Optimization arch_opt);
 
 } // namespace spaces
