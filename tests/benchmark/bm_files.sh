baae331f60f2e0802cfea170d43e5e9980eb4a18
meiravgri <meirav.grimberg@redis.com>
2022-12-22 10:29:19 +0200

    added default behavior for bm_files script

--- tests/benchmark/bm_files.sh
+++ tests/benchmark/bm_files.sh
@@ -1,6 +1,7 @@
 GIT_LABEL=$1
-fp32_labels="benchmarks-default, bm basics fp32 single, bm basics fp32 multi"
-if [ "$GIT_LABEL" = "bm-spaces" ]; then
+if [ -z "$GIT_LABEL"  ] || [ "$GIT_LABEL" = "benchmarks-all" ]; then 
+    file_name="all"
+elif [ "$GIT_LABEL" = "bm-spaces" ]; then
     :
 elif [ "$GIT_LABEL" = "benchmarks-default" ] \
 || [ "$GIT_LABEL" = "bm-basics-fp32-single" ] \
@@ -11,8 +12,6 @@ then
     file_name="basic_fp32"
 elif [ "$GIT_LABEL" = "bm-updated-fp32-single" ]; then
     file_name="updated"
-elif [ "$GIT_LABEL" = "benchmarks-all" ]; then
-    file_name="all"
 fi
 
 wget --no-check-certificate -q -i tests/benchmark/data/hnsw_indices/hnsw_indices_$file_name.txt -P tests/benchmark/data
