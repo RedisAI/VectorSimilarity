a97be5f544ab58c9801498f418671125be969c3e
meiravgri <109056284+meiravgri@users.noreply.github.com>
2022-12-24 07:51:45 +0200

    added default behavior for bm_files script (#312)
    * added default behavior for bm_files script
    
    * changed files script variable name to bm_type

--- tests/benchmark/bm_files.sh
+++ tests/benchmark/bm_files.sh
@@ -1,18 +1,17 @@
-GIT_LABEL=$1
-fp32_labels="benchmarks-default, bm basics fp32 single, bm basics fp32 multi"
-if [ "$GIT_LABEL" = "bm-spaces" ]; then
+BM_TYPE=$1
+if [ -z "$BM_TYPE"  ] || [ "$BM_TYPE" = "benchmarks-all" ]; then 
+    file_name="all"
+elif [ "$BM_TYPE" = "bm-spaces" ]; then
     :
-elif [ "$GIT_LABEL" = "benchmarks-default" ] \
-|| [ "$GIT_LABEL" = "bm-basics-fp32-single" ] \
-|| [ "$GIT_LABEL" = "bm-basics-fp32-multi" ] \
-|| [ "$GIT_LABEL" = "bm-batch-iter-fp32-single" ] \
-|| [ "$GIT_LABEL" = "bm-batch-iter-fp32-multi" ] 
+elif [ "$BM_TYPE" = "benchmarks-default" ] \
+|| [ "$BM_TYPE" = "bm-basics-fp32-single" ] \
+|| [ "$BM_TYPE" = "bm-basics-fp32-multi" ] \
+|| [ "$BM_TYPE" = "bm-batch-iter-fp32-single" ] \
+|| [ "$BM_TYPE" = "bm-batch-iter-fp32-multi" ] 
 then
     file_name="basic_fp32"
-elif [ "$GIT_LABEL" = "bm-updated-fp32-single" ]; then
+elif [ "$BM_TYPE" = "bm-updated-fp32-single" ]; then
     file_name="updated"
-elif [ "$GIT_LABEL" = "benchmarks-all" ]; then
-    file_name="all"
 fi
 
 wget --no-check-certificate -q -i tests/benchmark/data/hnsw_indices/hnsw_indices_$file_name.txt -P tests/benchmark/data
