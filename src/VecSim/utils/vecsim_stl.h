cb91c4cb8671fc90441d6f1748983375e769fbb8
meiravgri <meirav.grimberg@redis.com>
2023-01-10 09:50:48 +0000

    replacing ndoes mutex in one byte mutex

--- src/VecSim/utils/vecsim_stl.h
+++ src/VecSim/utils/vecsim_stl.h
@@ -91,4 +91,23 @@ public:
                                                                                        alloc) {}
 };
 
+struct one_byte_mutex {
+    void lock() {
+        if (state.exchange(locked, std::memory_order_acquire) == unlocked)
+            return;
+        while (state.exchange(sleeper, std::memory_order_acquire) != unlocked)
+            state.wait(sleeper, std::memory_order_relaxed);
+    }
+    void unlock() {
+        if (state.exchange(unlocked, std::memory_order_release) == sleeper)
+            state.notify_one();
+    }
+
+private:
+    std::atomic<uint8_t> state{unlocked};
+
+    static constexpr uint8_t unlocked = 0;
+    static constexpr uint8_t locked = 0b01;
+    static constexpr uint8_t sleeper = 0b10;
+};
 } // namespace vecsim_stl
