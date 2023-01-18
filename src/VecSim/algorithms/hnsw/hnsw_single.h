ea59e7d088ce0c96aa6b3cf2fe80bb359be42cf4
alon <alonreshef24@gmail.com>
2023-01-18 14:09:03 +0200

    format

--- src/VecSim/algorithms/hnsw/hnsw_single.h
+++ src/VecSim/algorithms/hnsw/hnsw_single.h
@@ -35,7 +35,8 @@ public:
         : HNSWIndex<DataType, DistType>(input, params, allocator, version),
           label_lookup_(this->max_elements_, allocator) {}
 
-    void getDataByLabel(labelType label, std::vector<std::vector<DataType>> &vectors_output) const override {
+    void getDataByLabel(labelType label,
+                        std::vector<std::vector<DataType>> &vectors_output) const override {
 
         auto id = label_lookup_.at(label);
 
