96f580699c2f927419e6504e8d27b3c3a5b915ed
GuyAv46 <guy.avimor@gmail.com>
2023-06-30 12:13:14 +0300

    tidy up

--- src/VecSim/spaces/spaces.cpp
+++ src/VecSim/spaces/spaces.cpp
@@ -10,47 +10,6 @@
 #include "VecSim/spaces/L2_space.h"
 namespace spaces {
 
-/*** Defined in spaces.h
- //optimization are defined according to
- // num_bits_per_iteration / bits_count_per_element
-    NO_OPTIMIZATION = 0,
-    SPLIT_TO_512_BITS = 1,          // FP32 -> dim % 16 == 0, FP64 -> dim % 8 == 0
-    SPLIT_TO_512_128_BITS = 2,           // FP32 -> dim % 4 == 0, FP64 -> dim % 2 == 0
-    SPLIT_TO_512_BITS_WITH_RESIDUALS = 3, //FP32 ->  dim > 16 && dim % 16 < 4, FP64 -> dim > 8 &&
-dim % 8 < 2, SPLIT_TO_512_128_BITS_WITH_RESIDUALS = 4,  // FP32 ->dim > 4, FP64 -> dim > 2
-***/
-CalculationGuideline FP32_GetCalculationGuideline(size_t dim) {
-
-    CalculationGuideline ret_score = NO_OPTIMIZATION;
-
-    if (dim % 16 == 0) {
-        ret_score = SPLIT_TO_512_BITS;
-    } else if (dim % 4 == 0) {
-        ret_score = SPLIT_TO_512_128_BITS;
-    } else if (dim > 16 && dim % 16 < 4) {
-        ret_score = SPLIT_TO_512_BITS_WITH_RESIDUALS;
-    } else if (dim > 4) {
-        ret_score = SPLIT_TO_512_128_BITS_WITH_RESIDUALS;
-    }
-    return ret_score;
-}
-
-CalculationGuideline FP64_GetCalculationGuideline(size_t dim) {
-
-    CalculationGuideline ret_score = NO_OPTIMIZATION;
-
-    if (dim % 8 == 0) {
-        ret_score = SPLIT_TO_512_BITS;
-    } else if (dim % 2 == 0) {
-        ret_score = SPLIT_TO_512_128_BITS;
-    } else if (dim > 8 && dim % 8 < 2) {
-        ret_score = SPLIT_TO_512_BITS_WITH_RESIDUALS;
-    } else if (dim > 2) {
-        ret_score = SPLIT_TO_512_128_BITS_WITH_RESIDUALS;
-    }
-    return ret_score;
-}
-
 void SetDistFunc(VecSimMetric metric, size_t dim, dist_func_t<float> *index_dist_func) {
 
     static const Arch_Optimization arch_opt = getArchitectureOptimization();
