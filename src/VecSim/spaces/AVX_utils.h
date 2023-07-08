baf58c0941022b5b4b2df373fd1a3a80e92f2656
GuyAv46 <47632673+GuyAv46@users.noreply.github.com>
2023-07-08 19:05:58 +0300

    Update AVX_utils.h comments

--- src/VecSim/spaces/AVX_utils.h
+++ src/VecSim/spaces/AVX_utils.h
@@ -7,19 +7,21 @@
 #pragma once
 #include "space_includes.h"
 
-template <__mmask8 mask> // 1..7
+template <__mmask8 mask> // (2^n)-1, where n is in 1..7 (1, 4, ..., 127)
 static inline __m256 my_mm256_maskz_loadu_ps(const float *p) {
+    // Load 8 floats (assuming this is safe to do)
     __m256 data = _mm256_loadu_ps(p);
-    // Set the mask for loading 8 float values (1 if mask is true, 0 if mask is false
+    // Set the mask for the loaded data (set 0 if a bit is 0)
     __m256 masked_data = _mm256_blend_ps(_mm256_setzero_ps(), data, mask);
 
     return masked_data;
 }
 
-template <__mmask8 mask> // 1..3
+template <__mmask8 mask> // (2^n)-1, where n is in 1..3 (1, 4, 7)
 static inline __m256d my_mm256_maskz_loadu_pd(const double *p) {
+    // Load 4 doubles (assuming this is safe to do)
     __m256d data = _mm256_loadu_pd(p);
-    // Set the mask for loading 8 float values (1 if mask is true, 0 if mask is false
+    // Set the mask for the loaded data (set 0 if a bit is 0)
     __m256d masked_data = _mm256_blend_pd(_mm256_setzero_pd(), data, mask);
 
     return masked_data;
