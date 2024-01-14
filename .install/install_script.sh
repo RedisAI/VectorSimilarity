67ba67b4cb50a33a7c5dc5de7d44921be4357ba1
GuyAv46 <guy.avimor@gmail.com>
2024-01-14 19:45:14 +0200

    fix names
    (cherry picked from commit 47952da339c3ef3d14b115dc6fd3240977f3beac)

--- .install/install_script.sh
+++ .install/install_script.sh
@@ -6,13 +6,13 @@ if [[ $OS_TYPE = 'Darwin' ]]
 then
     OS='macos'
 else
-    VERSION=$(grep '^VERSION_ID' /etc/os-release | sed 's/"//g')
+    VERSION=$(grep '^VERSION_ID=' /etc/os-release | sed 's/"//g')
     VERSION=${VERSION#"VERSION_ID="}
-    OS_NAME=$(grep '^NAME' /etc/os-release | sed 's/"//g')
+    OS_NAME=$(grep '^NAME=' /etc/os-release | sed 's/"//g')
     OS_NAME=${OS_NAME#"NAME="}
-    [[ OS_NAME == 'Rocky Linux' ]] && VERSION=${VERSION%.*} # Remove minor version
+    [[ $OS_NAME == 'Rocky Linux' ]] && VERSION=${VERSION%.*} # remove minor version for Rocky Linux
     OS=${OS_NAME,,}_${VERSION}
-    OS=${OS// /'_'}
+    OS=$(echo $OS | sed 's/[/ ]/_/g') # replace spaces and slashes with underscores
 fi
 echo $OS
 
