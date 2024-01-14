d8424bf494a2c3a2d8a539775bfa13486f1b4aa6
GuyAv46 <guy.avimor@gmail.com>
2024-01-14 20:07:50 +0200

    various fixes

--- .install/centos_linux_7.sh
+++ .install/centos_linux_7.sh
@@ -5,5 +5,5 @@ export DEBIAN_FRONTEND=noninteractive
 ARCH=$([[ $(uname -m) == x86_64 ]] && echo x86_64 || echo noarch)
 $MODE yum install -y https://packages.endpointdev.com/rhel/7/os/${ARCH}/endpoint-repo.${ARCH}.rpm
 $MODE yum groupinstall -y "Development Tools"
-$MODE yum install -y wget git valgrind
+$MODE yum install -y wget git valgrind devtoolset-10
 source install_cmake.sh $MODE
