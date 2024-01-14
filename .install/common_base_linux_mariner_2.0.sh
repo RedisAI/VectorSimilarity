010b9da4c49c1a0354afa03641664389d68607f0
GuyAv46 <guy.avimor@gmail.com>
2024-01-14 18:43:55 +0200

    added missing OSs we want to support
    (cherry picked from commit d358a9ba8a09569c2fc7e568e39b5fabf6736b58)

---
+++ .install/common_base_linux_mariner_2.0.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+MODE=$1 # whether to install using sudo or not
+set -e
+export DEBIAN_FRONTEND=noninteractive
+
+$MODE tdnf install -q -y build-essential git wget ca-certificates tar unzip rsync which valgrind
+
+source install_cmake.sh $MODE
