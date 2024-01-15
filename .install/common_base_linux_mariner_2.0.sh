4f4530cf23f01752e2d3b452507243cb3dbc6026
GuyAv46 <guy.avimor@gmail.com>
2024-01-15 11:06:51 +0200

    mariner

--- .install/common_base_linux_mariner_2.0.sh
+++ .install/common_base_linux_mariner_2.0.sh
@@ -3,6 +3,6 @@ MODE=$1 # whether to install using sudo or not
 set -e
 export DEBIAN_FRONTEND=noninteractive
 
-$MODE tdnf install -q -y build-essential git wget ca-certificates tar unzip rsync which valgrind
+$MODE tdnf install -q -y build-essential
 
 source install_cmake.sh $MODE
