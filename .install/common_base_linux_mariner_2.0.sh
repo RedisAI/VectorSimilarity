e73315448662a55bae4293f3c97109204c18e0c6
GuyAv46 <guy.avimor@gmail.com>
2024-01-15 11:13:17 +0200

    mariner
    (cherry picked from commit b7bc842196a3d4ca0e0fe8067a6d2c413465a22a)

--- .install/common_base_linux_mariner_2.0.sh
+++ .install/common_base_linux_mariner_2.0.sh
@@ -3,6 +3,6 @@ MODE=$1 # whether to install using sudo or not
 set -e
 export DEBIAN_FRONTEND=noninteractive
 
-$MODE tdnf install -q -y build-essential
+$MODE tdnf install -q -y build-essential git wget
 
 source install_cmake.sh $MODE
