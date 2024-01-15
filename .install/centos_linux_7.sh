39f0c9ec0197f6cbaa2b352e475d79100e47efac
GuyAv46 <guy.avimor@gmail.com>
2024-01-15 11:07:00 +0200

    centos
    (cherry picked from commit 4a609850d9a606d93d7d2c41e2689f482bc8243e)

--- .install/centos_linux_7.sh
+++ .install/centos_linux_7.sh
@@ -6,7 +6,7 @@ ARCH=$([[ $(uname -m) == x86_64 ]] && echo x86_64 || echo noarch)
 $MODE yum install -y https://packages.endpointdev.com/rhel/7/os/${ARCH}/endpoint-repo.${ARCH}.rpm
 $MODE yum groupinstall -y "Development Tools"
 $MODE yum install -y wget git valgrind centos-release-scl
-$MODE yum remove -y gcc g++ # remove gcc 4
 $MODE yum install -y devtoolset-10
 $MODE scl enable devtoolset-10 bash
+$MODE update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc10-gcc 60 --slave /usr/bin/g++ g++ /usr/bin/gcc10-g++
 source install_cmake.sh $MODE
