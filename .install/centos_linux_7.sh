694fb7ecd38e6f456ca17584dedec794836450c1
GuyAv46 <guy.avimor@gmail.com>
2024-01-15 11:29:05 +0200

    centos

--- .install/centos_linux_7.sh
+++ .install/centos_linux_7.sh
@@ -8,6 +8,7 @@ $MODE yum groupinstall -y "Development Tools"
 $MODE yum install -y wget git valgrind centos-release-scl
 $MODE yum install -y devtoolset-10
 $MODE scl enable devtoolset-10 bash
-$MODE update-alternatives --install /usr/bin/gcc gcc /opt/rh/devtoolset-10/root/usr/bin/gcc 60 \
-                            --slave /usr/bin/g++ g++ /opt/rh/devtoolset-10/root/usr/bin/g++
+$MODE yum remove -y gcc # remove gcc 4
+# $MODE update-alternatives --install /usr/bin/gcc gcc /opt/rh/devtoolset-10/root/usr/bin/gcc 60 \
+#                             --slave /usr/bin/g++ g++ /opt/rh/devtoolset-10/root/usr/bin/g++
 source install_cmake.sh $MODE
