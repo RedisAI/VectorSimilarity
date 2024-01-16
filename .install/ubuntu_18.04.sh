11392e4a102eb08a673211ddf0066e46a95d3437
GuyAv46 <47632673+GuyAv46@users.noreply.github.com>
2024-01-16 11:48:47 +0200

    CP and resolve conflicts

--- .install/ubuntu_18.04.sh
+++ .install/ubuntu_18.04.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 set -e
 export DEBIAN_FRONTEND=noninteractive
-apt-get update -qq
-apt-get upgrade -yqq
-apt-get dist-upgrade -yqq
-apt install -yqq software-properties-common
-add-apt-repository ppa:ubuntu-toolchain-r/test -y
-apt update
-apt-get install -yqq git wget make valgrind gcc-9 g++-9
-update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-9 60 --slave /usr/bin/g++ g++ /usr/bin/g++-9 
-source install_cmake.sh
+MODE=$1 # whether to install using sudo or not
+
+$MODE apt-get update -qq
+$MODE apt-get upgrade -yqq
+$MODE apt-get dist-upgrade -yqq
+$MODE apt install -yqq software-properties-common
+$MODE add-apt-repository ppa:ubuntu-toolchain-r/test -y
+$MODE apt update
+$MODE apt-get install -yqq git wget make valgrind gcc-9 g++-9
+$MODE update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-9 60 --slave /usr/bin/g++ g++ /usr/bin/g++-9
+source install_cmake.sh $MODE
