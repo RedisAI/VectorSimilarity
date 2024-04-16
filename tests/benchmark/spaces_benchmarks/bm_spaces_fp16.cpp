d17bebebc0e407aed72901c003bda7bb82c087d8
alon <alonreshef24@gmail.com>
2024-04-16 12:54:54 +0300

    format

--- tests/benchmark/spaces_benchmarks/bm_spaces_fp16.cpp
+++ tests/benchmark/spaces_benchmarks/bm_spaces_fp16.cpp
@@ -1,8 +1,8 @@
 /*
-*Copyright Redis Ltd. 2021 - present
-*Licensed under your choice of the Redis Source Available License 2.0 (RSALv2) or
-*the Server Side Public License v1 (SSPLv1).
-*/
+ *Copyright Redis Ltd. 2021 - present
+ *Licensed under your choice of the Redis Source Available License 2.0 (RSALv2) or
+ *the Server Side Public License v1 (SSPLv1).
+ */
 
 #include "VecSim/types/float16.h"
 #define DATA_TYPE vecsim_types::float16
