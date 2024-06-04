eea5f79e5fe98fd34bd1fbb1297af3b29c29430e
meiravgri <meirav.grimberg@redis.com>
2024-06-04 13:27:07 +0000

    rocky8: try update alternatives

--- .install/rocky_linux_8.sh
+++ .install/rocky_linux_8.sh
@@ -17,6 +17,4 @@ $MODE dnf install epel-release -yqq
 
 $MODE dnf install -y gcc-toolset-11-gcc gcc-toolset-11-gcc-c++ make valgrind wget git
 
-$MODE source /opt/rh/gcc-toolset-11/enable
-
-source install_cmake.sh $MODE
+$MODE update-alternatives --install /usr/bin/gcc gcc /opt/rh/gcc-toolset-11/root/usr/bin/gcc 60 --slave   /usr/bin/g++ g++ /opt/rh/gcc-toolset-11/root/usr/bin/g++
