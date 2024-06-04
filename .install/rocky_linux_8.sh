2ac3d25c631f30599bd6554861a2d2922d854c89
meiravgri <meirav.grimberg@redis.com>
2024-06-04 13:11:56 +0000

    fix rocky8 installation

--- .install/rocky_linux_8.sh
+++ .install/rocky_linux_8.sh
@@ -15,9 +15,8 @@ $MODE dnf config-manager --set-enabled powertools
 # get epel to install gcc11
 $MODE dnf install epel-release -yqq
 
-$MODE dnf install -y gcc-toolset-11-gcc gcc-toolset-11-gcc-c++ gcc-toolset-11-libatomic-devel make wget git openssl openssl-devel \
-    bzip2-devel libffi-devel zlib-devel tar xz which rsync
+$MODE dnf install -y gcc-toolset-11-gcc gcc-toolset-11-gcc-c++ make valgrind wget git
 
-cp /opt/rh/gcc-toolset-11/enable /etc/profile.d/gcc-toolset-11.sh
+$MODE source /opt/rh/gcc-toolset-11/enable
 
 source install_cmake.sh $MODE
