b0d85e518eba0495ff744495f9fdd6438a1b30b3
meiravgri <meirav.grimberg@redis.com>
2024-06-04 12:27:47 +0000

    upgrade to gcc11 on rocky8, boinic and focal

--- .install/ubuntu_18.04.sh
+++ .install/ubuntu_18.04.sh
@@ -9,8 +9,8 @@ $MODE apt-get dist-upgrade -yqq
 $MODE apt install -yqq software-properties-common
 $MODE add-apt-repository ppa:ubuntu-toolchain-r/test -y
 $MODE apt update
-$MODE apt-get install -yqq git wget make gcc-9 g++-9 libc6-dbg
-$MODE update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-9 60 --slave /usr/bin/g++ g++ /usr/bin/g++-9
+$MODE apt-get install -yqq git wget make gcc-11 g++-11 libc6-dbg
+$MODE update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-11 60 --slave /usr/bin/g++ g++ /usr/bin/g++-11
 
 wget https://sourceware.org/pub/valgrind/valgrind-3.18.0.tar.bz2
 tar -xjf valgrind-3.18.0.tar.bz2
