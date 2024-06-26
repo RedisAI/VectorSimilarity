35775dff14ba058b1507148ad892f3e6b1a496a2
meiravgri <meirav.grimberg@redis.com>
2024-06-26 12:23:12 +0000

    more fixes

--- tests/benchmark/spaces_benchmarks/bm_spaces_fp16.cpp
+++ tests/benchmark/spaces_benchmarks/bm_spaces_fp16.cpp
@@ -13,8 +13,8 @@ cpu_features::X86Features opt = cpu_features::GetX86Info().features;
 
 // AVX512_BW_VL functions
 #ifdef OPT_AVX512F
-bool avx512_supported = opt.avx512f;
-INITIALIZE_BENCHMARKS_SET(FP16, AVX512, 32, avx512_supported);
+bool avx512f_supported = opt.avx512f;
+INITIALIZE_BENCHMARKS_SET(FP16, AVX512F, 32, avx512f_supported);
 #endif // OPT_AVX512F
 
 // AVX functions
--- tests/benchmark/spaces_benchmarks/bm_spaces_fp64.cpp
+++ tests/benchmark/spaces_benchmarks/bm_spaces_fp64.cpp
@@ -11,8 +11,8 @@ cpu_features::X86Features opt = cpu_features::GetX86Info().features;
 
 // AVX512 functions
 #ifdef OPT_AVX512F
-bool avx512_supported = opt.avx512f;
-INITIALIZE_BENCHMARKS_SET(FP64, AVX512, 8, avx512_supported);
+bool avx512f_supported = opt.avx512f;
+INITIALIZE_BENCHMARKS_SET(FP64, AVX512F, 8, avx512f_supported);
 #endif // AVX512F
 
 // AVX functions
