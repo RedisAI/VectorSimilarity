5fe358db632e79cf7e7b378b194fdf3bf944a75f
alonre24 <alonreshef24@gmail.com>
2024-07-02 12:43:57 +0300

    revert file name

--- tests/benchmark/bm_vecsim_general.h
+++ tests/benchmark/bm_vecsim_general.h
@@ -79,6 +79,6 @@ protected:
 
     // Adds the library's root path to @file_name
     static inline std::string AttachRootPath(std::string file_name) {
-        return std::string("/home/alon/Code/VectorSimilarity") + "/" + file_name;
+        return std::string(getenv("ROOT")) + "/" + file_name;
     }
 };
