1e6bc9aad307ac7886199a29aaa9242a4eed7a7c
meiravgri <109056284+meiravgri@users.noreply.github.com>
2024-07-03 17:28:55 +0300

    Re-design benchmark class: (#479)
    now benchmark is templated
    each type inherites the base class and modified the conversion method if needed.

--- tests/benchmark/spaces_benchmarks/bm_spaces_fp16.cpp
+++ tests/benchmark/spaces_benchmarks/bm_spaces_fp16.cpp
@@ -5,8 +5,13 @@
  */
 
 #include "VecSim/types/float16.h"
-#define DATA_TYPE vecsim_types::float16
 #include "bm_spaces.h"
+
+class BM_VecSimSpaces_FP16 : public BM_VecSimSpaces<vecsim_types::float16> {
+    vecsim_types::float16 DoubleToType(double val) override {
+        return vecsim_types::FP32_to_FP16(val);
+    }
+};
 
 #ifdef CPU_FEATURES_ARCH_X86_64
 cpu_features::X86Features opt = cpu_features::GetX86Info().features;
@@ -14,18 +19,18 @@ cpu_features::X86Features opt = cpu_features::GetX86Info().features;
 // AVX512_BW_VL functions
 #ifdef OPT_AVX512F
 bool avx512_supported = opt.avx512f;
-INITIALIZE_BENCHMARKS_SET(FP16, AVX512, 32, avx512_supported);
+INITIALIZE_BENCHMARKS_SET(BM_VecSimSpaces_FP16, FP16, AVX512, 32, avx512_supported);
 #endif // OPT_AVX512F
 
 // AVX functions
 #ifdef OPT_F16C
 bool avx512_bw_f16c_supported = opt.f16c && opt.fma3 && opt.avx;
-INITIALIZE_BENCHMARKS_SET(FP16, F16C, 32, avx512_bw_f16c_supported);
+INITIALIZE_BENCHMARKS_SET(BM_VecSimSpaces_FP16, FP16, F16C, 32, avx512_bw_f16c_supported);
 #endif // OPT_F16C
 
 #endif // x86_64
 
-INITIALIZE_NAIVE_BM(FP16, InnerProduct, 32);
-INITIALIZE_NAIVE_BM(FP16, L2Sqr, 32);
+INITIALIZE_NAIVE_BM(BM_VecSimSpaces_FP16, FP16, InnerProduct, 32);
+INITIALIZE_NAIVE_BM(BM_VecSimSpaces_FP16, FP16, L2Sqr, 32);
 
 BENCHMARK_MAIN();
