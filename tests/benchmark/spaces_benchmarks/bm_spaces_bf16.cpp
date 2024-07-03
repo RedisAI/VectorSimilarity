c4929e12eb6d33de700a95ca5453dc22924a7a9b
meiravgri <meirav.grimberg@redis.com>
2024-07-03 12:14:57 +0000

    Re-design benchmark class: now benchmark is templated each type inherites the base class and modified the conversion method if needed.
    (cherry picked from commit 89e4703d98946016e2142215942d9a511f1fdd2b)

--- tests/benchmark/spaces_benchmarks/bm_spaces_bf16.cpp
+++ tests/benchmark/spaces_benchmarks/bm_spaces_bf16.cpp
@@ -5,8 +5,13 @@
  */
 
 #include "VecSim/types/bfloat16.h"
-#define DATA_TYPE vecsim_types::bfloat16
 #include "bm_spaces.h"
+
+class BM_VecSimSpaces_BF16 : public BM_VecSimSpaces<vecsim_types::bfloat16> {
+    vecsim_types::bfloat16 DoubleToType(double val) override {
+        return vecsim_types::float_to_bf16(val);
+    }
+};
 
 #ifdef CPU_FEATURES_ARCH_X86_64
 cpu_features::X86Features opt = cpu_features::GetX86Info().features;
@@ -14,29 +19,31 @@ cpu_features::X86Features opt = cpu_features::GetX86Info().features;
 // AVX512_BF16 functions
 #ifdef OPT_AVX512_BF16_VL
 bool avx512_bf16_vl_supported = opt.avx512_bf16 && opt.avx512vl;
-INITIALIZE_BENCHMARKS_SET_IP(BF16, AVX512BF16_VL, 32, avx512_bf16_vl_supported);
+INITIALIZE_BENCHMARKS_SET_IP(BM_VecSimSpaces_BF16, BF16, AVX512BF16_VL, 32,
+                             avx512_bf16_vl_supported);
 #endif // AVX512_BF16
 
 // AVX512 functions
 #ifdef OPT_AVX512_BW_VBMI2
 bool avx512_bw_vbmi2_supported = opt.avx512bw && opt.avx512vbmi2;
-INITIALIZE_BENCHMARKS_SET(BF16, AVX512BW_VBMI2, 32, avx512_bw_vbmi2_supported);
+INITIALIZE_BENCHMARKS_SET(BM_VecSimSpaces_BF16, BF16, AVX512BW_VBMI2, 32,
+                          avx512_bw_vbmi2_supported);
 #endif // AVX512F
 
 // AVX functions
 #ifdef OPT_AVX2
 bool avx2_supported = opt.avx2;
-INITIALIZE_BENCHMARKS_SET(BF16, AVX2, 32, avx2_supported);
+INITIALIZE_BENCHMARKS_SET(BM_VecSimSpaces_BF16, BF16, AVX2, 32, avx2_supported);
 #endif // AVX
 
 // SSE functions
 #ifdef OPT_SSE3
 bool sse3_supported = opt.sse3;
-INITIALIZE_BENCHMARKS_SET(BF16, SSE3, 32, sse3_supported);
+INITIALIZE_BENCHMARKS_SET(BM_VecSimSpaces_BF16, BF16, SSE3, 32, sse3_supported);
 #endif // SSE
 
 #endif // x86_64
 
-INITIALIZE_NAIVE_BM(BF16, InnerProduct_LittleEndian, 32);
-INITIALIZE_NAIVE_BM(BF16, L2Sqr_LittleEndian, 32);
+INITIALIZE_NAIVE_BM(BM_VecSimSpaces_BF16, BF16, InnerProduct_LittleEndian, 32);
+INITIALIZE_NAIVE_BM(BM_VecSimSpaces_BF16, BF16, L2Sqr_LittleEndian, 32);
 BENCHMARK_MAIN();
