bc2e91885ef86dcc9f988e932e529249b281e918
meiravgri <meirav.grimberg@redis.com>
2024-07-03 05:40:30 +0000

    install gcc12 ubuntu22

--- .install/ubuntu_22.04.sh
+++ .install/ubuntu_22.04.sh
@@ -4,5 +4,5 @@ export DEBIAN_FRONTEND=noninteractive
 MODE=$1 # whether to install using sudo or not
 
 $MODE apt-get update -qq || true
-$MODE apt-get install -yqq git wget build-essential valgrind lcov
+$MODE apt-get install -yqq gcc-12 g++-12 git wget build-essential valgrind lcov
 source install_cmake.sh $MODE
