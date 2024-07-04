17ad88d8553864e90c10e481a9245b244de7d7eb
meiravgri <meirav.grimberg@redis.com>
2024-07-04 10:50:47 +0000

    jammy: revedrt to gcc12

--- .install/ubuntu_22.04.sh
+++ .install/ubuntu_22.04.sh
@@ -4,6 +4,6 @@ export DEBIAN_FRONTEND=noninteractive
 MODE=$1 # whether to install using sudo or not
 
 $MODE apt-get update -qq || true
-$MODE apt-get install -yqq gcc-13 g++-13 git wget build-essential valgrind lcov
-$MODE update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-13 60 --slave /usr/bin/g++ g++ /usr/bin/g++-13
+$MODE apt-get install -yqq gcc-12 g++-12 git wget build-essential valgrind lcov
+$MODE update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-12 60 --slave /usr/bin/g++ g++ /usr/bin/g++-12
 source install_cmake.sh $MODE
