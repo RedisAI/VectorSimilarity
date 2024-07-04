691b05c6f406b3e5954eb3bcf2521b5bb02a4260
meiravgri <meirav.grimberg@redis.com>
2024-07-04 08:43:13 +0000

    Upgrade gcc in rocky 8 (12) + 9 (13)

--- .install/rocky_linux_8.sh
+++ .install/rocky_linux_8.sh
@@ -11,11 +11,11 @@ $MODE dnf groupinstall "Development Tools" -yqq
 # powertools is needed to install epel
 $MODE dnf config-manager --set-enabled powertools
 
-# get epel to install gcc11
+# get epel to install gcc12
 $MODE dnf install epel-release -yqq
 
-$MODE dnf install -y gcc-toolset-11-gcc gcc-toolset-11-gcc-c++ gcc-toolset-11-libatomic-devel  make valgrind wget git
+$MODE dnf install -y gcc-toolset-12-gcc gcc-toolset-12-gcc-c++ gcc-toolset-12-libatomic-devel  make valgrind wget git
 
-cp /opt/rh/gcc-toolset-11/enable /etc/profile.d/gcc-toolset-11.sh
+cp /opt/rh/gcc-toolset-12/enable /etc/profile.d/gcc-toolset-12.sh
 
 source install_cmake.sh $MODE
