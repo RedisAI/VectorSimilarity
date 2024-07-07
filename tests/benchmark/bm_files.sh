c36b6231b368a73be6fcd0c41c272f97a091d1d7
meiravgri <meirav.grimberg@redis.com>
2024-07-07 10:46:44 +0000

    download in parallel

--- tests/benchmark/bm_files.sh
+++ tests/benchmark/bm_files.sh
@@ -32,4 +32,5 @@ elif [ "$BM_TYPE" = "bm-updated-fp32-single" ]; then
     file_name="updated"
 fi
 
-wget --no-check-certificate -q -i tests/benchmark/data/hnsw_indices/hnsw_indices_$file_name.txt -P tests/benchmark/data
+# wget --no-check-certificate -q -i tests/benchmark/data/hnsw_indices/hnsw_indices_$file_name.txt -P tests/benchmark/data
+cat tests/benchmark/data/hnsw_indices/hnsw_indices_$file_name.txt | xargs -n 1 -P 10 wget --no-check-certificate -q -P tests/benchmark/data
