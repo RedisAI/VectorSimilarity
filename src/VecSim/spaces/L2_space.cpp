0c40404558100bb4ac9e89245bbb862f060e1db3
meiravgri <meirav.grimberg@redis.com>
2024-07-07 13:13:32 +0000

    use explicit ctor for bfloat to avoid implicit casts

--- src/VecSim/spaces/L2_space.cpp
+++ src/VecSim/spaces/L2_space.cpp
@@ -163,6 +163,8 @@ dist_func_t<float> L2_FP16_GetDistFunc(size_t dim, unsigned char *alignment, con
                         ? cpu_features::GetX86Info().features
                         : *static_cast<const cpu_features::X86Features *>(arch_opt);
 #ifdef OPT_AVX512_FP16
+    // More details about the dimension limitation can be found in this PR's description:
+    // https://github.com/RedisAI/VectorSimilarity/pull/477
     if (features.avx512_fp16 && dim >= 440) {
         if (dim % 32 == 0) // no point in aligning if we have an offsetting residual
             *alignment = 32 * sizeof(float16); // handles 32 floats
