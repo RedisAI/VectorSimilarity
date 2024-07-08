daffdcd59f4bd9928e26d9026df30e8372f08663
meiravgri <meirav.grimberg@redis.com>
2024-07-08 16:40:53 +0300

    exit if no file

--- tests/benchmark/bm_files.sh
+++ tests/benchmark/bm_files.sh
@@ -1,8 +1,6 @@
 BM_TYPE=$1
 if [ -z "$BM_TYPE"  ] || [ "$BM_TYPE" = "benchmarks-all" ]; then
     file_name="all"
-elif [ "$BM_TYPE" = "bm-spaces" ]; then
-    exit 0
 elif [ "$BM_TYPE" = "benchmarks-default" ] \
 || [ "$BM_TYPE" = "bm-basics-fp32-single" ] \
 || [ "$BM_TYPE" = "bm-basics-fp32-multi" ] \
@@ -30,6 +28,8 @@ then
     file_name="basic_fp16"
 elif [ "$BM_TYPE" = "bm-updated-fp32-single" ]; then
     file_name="updated"
+else
+    echo "No files to download for BM_TYPE=$BM_TYPE"
 fi
 
 cat tests/benchmark/data/hnsw_indices/hnsw_indices_$file_name.txt | xargs -n 1 -P 0 wget --no-check-certificate -P tests/benchmark/data
