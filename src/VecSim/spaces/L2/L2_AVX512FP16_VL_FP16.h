3d77d233fe3872a1e9ebac847c2eb25daa21ec91
meiravgri <109056284+meiravgri@users.noreply.github.com>
2024-08-13 13:44:57 +0300

    [MOD-7369] Add AVX512VL flag to FP16 flag to enhance performance. (#522)
    * add AVX512VL flag to FP16 flag to enhance performance.
    
    With this flag the dimension limit to chose the advanced opt is not required.
    
    Expanded low dim bm spaces range.
    
    * add VL to building with prompt in compilation
    
    * remove old file

--- src/VecSim/spaces/L2/L2_AVX512FP16_VL_FP16.h
+++ src/VecSim/spaces/L2/L2_AVX512FP16_VL_FP16.h
@@ -23,7 +23,7 @@ static inline void L2SqrStep(float16 *&pVect1, float16 *&pVect2, __m512h &sum) {
 }
 
 template <unsigned short residual> // 0..31
-float FP16_L2SqrSIMD32_AVX512FP16(const void *pVect1v, const void *pVect2v, size_t dimension) {
+float FP16_L2SqrSIMD32_AVX512FP16_VL(const void *pVect1v, const void *pVect2v, size_t dimension) {
     auto *pVect1 = (float16 *)pVect1v;
     auto *pVect2 = (float16 *)pVect2v;
 
