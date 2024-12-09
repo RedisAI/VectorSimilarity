efbebe3a685429013393b549b9515075fa69d8a2
lerman25 <lerman25@gmail.com>
2024-12-09 15:32:15 +0200

    fix blend

--- src/VecSim/spaces/normalize/normalize_AVX_FP32.h
+++ src/VecSim/spaces/normalize/normalize_AVX_FP32.h
@@ -75,12 +75,12 @@ void FP32_normIMD16_AVX(const void *pVect1v,size_t dimension) {
     // Deal with 1-7 floats with mask loading, if needed
     if constexpr (residual % 8) {
         __mmask8 constexpr mask8 = (1 << (residual % 8)) - 1;
-        __m256 v1 = my_mm256_maskz_loadu_ps<mask8>(pVect1);
-        _mm256_storeu_ps(pVect1,_mm256_div_ps (v1,normFactor));
+        __m256 v1 = _mm256_loadu_ps(pVect1);
+        __m256 blend = _mm256_blend_ps(v1,_mm256_div_ps(v1,normFactor),mask8);
+        _mm256_storeu_ps(pVect1,blend);
 
         pVect1 += residual % 8;
     }
-
     // If the reminder is >=8, have another step of 8 floats
     if constexpr (residual >= 8) {
         divStep(pVect1,normFactor);
