736e30b99fb7afd4a5430518b125471c139d60bb
meiravgri <meirav.grimberg@redis.com>
2024-12-18 12:11:29 +0000

    use stack allocation instead of heap allocation in tests

--- tests/unit/test_spaces.cpp
+++ tests/unit/test_spaces.cpp
@@ -242,8 +242,8 @@ TEST_F(SpacesTest, int8_ip_no_optimization_func_test) {
 TEST_F(SpacesTest, int8_Cosine_no_optimization_func_test) {
     size_t dim = 4;
     // create a vector with extra space for the norm
-    int8_t *v1 = new int8_t[dim + sizeof(float)];
-    int8_t *v2 = new int8_t[dim + sizeof(float)];
+    int8_t v1[dim + sizeof(float)];
+    int8_t v2[dim + sizeof(float)];
 
     test_utils::populate_int8_vec(v1, dim, 123);
     test_utils::populate_int8_vec(v2, dim, 123);
@@ -917,8 +917,8 @@ class INT8SpacesOptimizationTest : public testing::TestWithParam<size_t> {};
 TEST_P(INT8SpacesOptimizationTest, INT8L2SqrTest) {
     auto optimization = cpu_features::GetX86Info().features;
     size_t dim = GetParam();
-    int8_t *v1 = new int8_t[dim];
-    int8_t *v2 = new int8_t[dim];
+    int8_t v1[dim];
+    int8_t v2[dim];
     test_utils::populate_int8_vec(v1, dim, 123);
     test_utils::populate_int8_vec(v2, dim, 1234);
 
@@ -953,8 +953,8 @@ TEST_P(INT8SpacesOptimizationTest, INT8L2SqrTest) {
 TEST_P(INT8SpacesOptimizationTest, INT8InnerProductTest) {
     auto optimization = cpu_features::GetX86Info().features;
     size_t dim = GetParam();
-    int8_t *v1 = new int8_t[dim];
-    int8_t *v2 = new int8_t[dim];
+    int8_t v1[dim];
+    int8_t v2[dim];
     test_utils::populate_int8_vec(v1, dim, 123);
     test_utils::populate_int8_vec(v2, dim, 1234);
 
@@ -990,8 +990,8 @@ TEST_P(INT8SpacesOptimizationTest, INT8InnerProductTest) {
 TEST_P(INT8SpacesOptimizationTest, INT8CosineTest) {
     auto optimization = cpu_features::GetX86Info().features;
     size_t dim = GetParam();
-    int8_t *v1 = new int8_t[dim + sizeof(float)];
-    int8_t *v2 = new int8_t[dim + sizeof(float)];
+    int8_t v1[dim + sizeof(float)];
+    int8_t v2[dim + sizeof(float)];
     test_utils::populate_int8_vec(v1, dim, 123);
     test_utils::populate_int8_vec(v2, dim, 1234);
 
