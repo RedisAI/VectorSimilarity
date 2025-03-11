1ca2ddf6c8b04b011e1ffede93c8ff5f976d7dc0
lerman25 <lerman25@gmail.com>
2025-03-11 15:09:50 +0200

    add comment

--- tests/benchmark/types_ranges.h
+++ tests/benchmark/types_ranges.h
@@ -1,9 +1,12 @@
+#pragma once
 #include <array>
 #include "bm_definitions.h"
 
 #define DEFAULT_RANGE_RADII {20,35,50}
 #define DEFAULT_RANGE_EPSILONS {1,10,11}
 
+// This template struct methods returns the default values for radii and epsilons
+// To specify different values for a certain type, use template specialization
 template<typename t>
 struct benchmark_range {
     static std::array<unsigned int,3> get_radii() {
@@ -14,8 +17,8 @@ struct benchmark_range {
     }
 };
 
-// Larger Range query values are required for wikipedia dataset.
-// Current values in bm_vecsim_basics.h gives 0 results
+// Larger Range query values are required for int8 wikipedia dataset.
+// Default values gives 0 results
 #define INT8_RANGE_RADII {50,65,80}
 
 template<>
