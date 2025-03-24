b76b9fbbba8cc2f314f51e5e15b1db88d298a97b
BenGoldberger <ben.goldberger@redis.com>
2025-03-24 17:13:41 +0200

    try to get arm opt to work

--- src/VecSim/spaces/L2/L2_SVE_FP64.h
+++ src/VecSim/spaces/L2/L2_SVE_FP64.h
@@ -25,8 +25,8 @@ static void L2SquareStep(double *&pVect1, double *&pVect2, svfloat64_t &sum) {
 
 template <unsigned char residual>
 double FP64_L2SqrSIMD_SVE(const void *pVect1v, const void *pVect2v, size_t dimension) {
-    const double *pVect1 = (double*) pVect1v;
-    const double *pVect2 = static_cast<const double *>(pVect2v);
+    double *pVect1 = (double*) pVect1v;
+    double *pVect2 = static_cast<const double *>(pVect2v);
 
     // Get the number of 64-bit elements per vector at runtime
     uint64_t vl = svcntd();
