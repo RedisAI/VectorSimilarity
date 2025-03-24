87feb67ad6e48fc70832555b575fd629811fd5fe
Dor Forer <dor.forer@redis.com>
2025-03-24 16:14:54 +0200

    Revert implemetion chooser

--- src/VecSim/spaces/functions/implementation_chooser.h
+++ src/VecSim/spaces/functions/implementation_chooser.h
@@ -32,7 +32,6 @@
 
 // Macros for 8, 16, 32 and 64 cases. Used to collapse the switch statement.
 // Expands into 0-7, 0-15, 0-31 or 0-63 cases respectively.
-#define CASES1(func)  C1(func, 1)
 #define CASES8(func)  C8(func, 0)
 #define CASES16(func) C16(func, 0)
 #define CASES32(func) C32(func, 0)
@@ -51,11 +50,6 @@
 #define CHOOSE_IMPLEMENTATION(out, dim, chunk, func)                                               \
     do {                                                                                           \
         decltype(out) __ret_dist_func;                                                             \
-        if ((chunk) == 1) {                                                                        \
-            /* Handle the case where chunk is 0 */                                                 \
-            __ret_dist_func = func<1>;                                                             \
-        } else {                                                                                   \
-            switch ((dim) % (chunk)) { CASES##chunk(func) }                                        \
-        }                                                                                          \
+        switch ((dim) % (chunk)) { CASES##chunk(func) }                                            \
         out = __ret_dist_func;                                                                     \
     } while (0)
