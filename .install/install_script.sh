20b564a774b0ab6a97129f0dda0e2b0fe616b252
Dor Forer <dor.forer@redis.com>
2025-03-30 11:43:23 +0300

    pr

--- .install/install_script.sh
+++ .install/install_script.sh
@@ -16,6 +16,7 @@ else
     OS=$(echo $OS | sed 's/[/ ]/_/g') # replace spaces and slashes with underscores
 fi
 echo $OS
+
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 cd $SCRIPT_DIR
