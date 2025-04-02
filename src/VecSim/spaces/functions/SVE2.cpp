bd30cb6aefb3486602747b0a32c69ee3a63497c3
BenGoldberger <ben.goldberger@redis.com>
2025-04-02 10:18:37 +0300

    pr changes

--- src/VecSim/spaces/functions/SVE2.cpp
+++ src/VecSim/spaces/functions/SVE2.cpp
@@ -23,7 +23,7 @@ dist_func_t<float> Choose_FP32_L2_implementation_SVE2(size_t dim) {
     CHOOSE_SVE_IMPLEMENTATION(ret_dist_func, FP32_L2SqrSIMD_SVE, dim, svcntw);
     return ret_dist_func;
 }
-dist_func_t<double> Choose_FP64_IP_implementation_SVE(size_t dim) {
+dist_func_t<double> Choose_FP64_IP_implementation_SVE2(size_t dim) {
     dist_func_t<double> ret_dist_func;
     CHOOSE_SVE_IMPLEMENTATION(ret_dist_func, FP64_InnerProductSIMD_SVE, dim, svcntd);
     return ret_dist_func;
