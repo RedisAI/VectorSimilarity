8cf539aa2f59a18ea48ef1db3fd3ca3e5398902a
meiravgri <109056284+meiravgri@users.noreply.github.com>
2025-04-02 13:29:31 +0300

    Pin cmake version before brew update (#624)

--- .install/macos.sh
+++ .install/macos.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+
+# Without pinning cmake, it will install the latest version(>= 4.0)
+# This leads to to an error:
+# Compatibility with CMake < 3.5 has been removed from CMake.
+# For now we went with pinning cmake to 3.31.6 which is the version that is exists in the current mac OS docker image we use
+brew pin cmake
 brew update
 brew install make
+brew install coreutils
 source install_cmake.sh
