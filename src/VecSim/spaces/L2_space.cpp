8ca501b21013abb939391a47eb378994978f22b5
Dor Forer <dor.forer@redis.com>
2025-04-06 12:04:22 +0300

    changes

--- src/VecSim/spaces/L2_space.cpp
+++ src/VecSim/spaces/L2_space.cpp
@@ -52,11 +52,11 @@ dist_func_t<float> L2_FP32_GetDistFunc(size_t dim, const Arch_Optimization arch_
 #endif
 #endif
 
-#ifdef CPU_FEATURES_ARCH_X86_64
 
-    CalculationGuideline optimization_type = FP32_GetCalculationGuideline(dim);
+CalculationGuideline optimization_type = FP32_GetCalculationGuideline(dim);
 
     switch (arch_opt) {
+#ifdef CPU_FEATURES_ARCH_X86_64
     case ARCH_OPT_AVX512_DQ:
     case ARCH_OPT_AVX512_F:
 
@@ -98,6 +98,24 @@ dist_func_t<float> L2_FP32_GetDistFunc(size_t dim, const Arch_Optimization arch_
         ret_dist_func = dist_funcs[optimization_type];
     } break;
 #endif
+#endif
+#ifdef CPU_FEATURES_ARCH_AARCH64
+    case ARCH_OPT_SVE2:
+#ifdef OPT_SVE2
+        ret_dist_func = Choose_FP32_L2_implementation_SVE2(dim);
+        break;
+#endif
+    case ARCH_OPT_SVE:
+#ifdef OPT_SVE
+        ret_dist_func = Choose_FP32_L2_implementation_SVE(dim);
+        break;
+#endif
+    case ARCH_OPT_NEON:
+#ifdef OPT_NEON
+        ret_dist_func = Choose_FP32_L2_implementation_NEON(dim);
+        break;
+#endif
+#endif // CPU_FEATURES_ARCH_AARCH64
     case ARCH_OPT_NONE:
         break;
     } // switch
