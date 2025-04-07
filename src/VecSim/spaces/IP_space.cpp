4fb3da973451c693685f9e79a472e03340745105
Dor Forer <dor.forer@redis.com>
2025-04-07 11:46:16 +0300

    format

--- src/VecSim/spaces/IP_space.cpp
+++ src/VecSim/spaces/IP_space.cpp
@@ -65,18 +65,18 @@ dist_func_t<float> IP_FP32_GetDistFunc(size_t dim, const Arch_Optimization arch_
 #ifdef CPU_FEATURES_ARCH_AARCH64
     case ARCH_OPT_SVE2:
 #ifdef OPT_SVE2
-    ret_dist_func = Choose_FP32_IP_implementation_SVE2(dim);
-    break;
+        ret_dist_func = Choose_FP32_IP_implementation_SVE2(dim);
+        break;
 #endif
     case ARCH_OPT_SVE:
 #ifdef OPT_SVE
-    ret_dist_func = Choose_FP32_IP_implementation_SVE(dim);
-    break;
+        ret_dist_func = Choose_FP32_IP_implementation_SVE(dim);
+        break;
 #endif
     case ARCH_OPT_NEON:
 #ifdef OPT_NEON
-    ret_dist_func = Choose_FP32_IP_implementation_NEON(dim);
-    break;
+        ret_dist_func = Choose_FP32_IP_implementation_NEON(dim);
+        break;
 #endif
 #endif // CPU_FEATURES_ARCH_X86_64
     case ARCH_OPT_NONE:
