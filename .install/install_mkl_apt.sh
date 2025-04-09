0d40e5f7a586915cfa69a16fe66eb202634500bc
Rafik Saliev <rafik.f.saliev@intel.com>
2025-04-09 10:46:00 +0200

    Reduce MKL size in Ubuntu by using 'core' package.

--- .install/install_mkl_apt.sh
+++ .install/install_mkl_apt.sh
@@ -11,6 +11,6 @@ then
         wget -O- https://apt.repos.intel.com/intel-gpg-keys/GPG-PUB-KEY-INTEL-SW-PRODUCTS.PUB | gpg --dearmor | $MODE tee /usr/share/keyrings/oneapi-archive-keyring.gpg > /dev/null
         echo "deb [signed-by=/usr/share/keyrings/oneapi-archive-keyring.gpg] https://apt.repos.intel.com/oneapi all main" | $MODE tee /etc/apt/sources.list.d/intel-oneapi.list
         $MODE apt update -qq || true
-        $MODE apt install -yqq intel-oneapi-mkl-devel-${version}
+        $MODE apt install -yqq intel-oneapi-mkl-core-devel-${version}
     fi
 fi
