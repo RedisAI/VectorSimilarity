2138ca594ae6e2e62250ececc2e4cc06c6cb6d18
Rafik Saliev <rafik.f.saliev@intel.com>
2025-04-16 16:38:24 +0200

    Address code review comments

--- cmake/svs.cmake
+++ cmake/svs.cmake
@@ -4,11 +4,6 @@ if(POLICY CMP0135)
     cmake_policy(SET CMP0135 NEW)
 endif()
 
-# option(SVS_EXPERIMENTAL_LEANVEC "Enable experimental LeanVec in SVS" YES)
-# if(SVS_EXPERIMENTAL_LEANVEC)
-#     add_definitions(-DSVS_EXPERIMENTAL_LEANVEC)
-# endif()
-
 # Valgrind does not support AVX512 and Valgrind in running in Debug
 # so disable it if we are in Debug mode
 string(TOUPPER "${CMAKE_BUILD_TYPE}" uppercase_CMAKE_BUILD_TYPE)
@@ -34,30 +29,34 @@ endif()
 
 include(CMakeDependentOption)
 
-# USE_SVS option forsibly OFF if CPU or compiler is not supported
+# USE_SVS option forcibly OFF if CPU or compiler is not supported
 # elsewhere let user disable SVS
 cmake_dependent_option(USE_SVS "Build with SVS library support" ON "SVS_SUPPORTED" OFF)
 if(USE_SVS)
-    message(STATUS "SVS support enabled")
 else()
-    message(STATUS "SVS support disabled")
 endif()
 
-# try to find MKL
-set(MKL_HINTS ${MKLROOT} $ENV{MKL_DIR} $ENV{MKL_ROOT} $ENV{MKLROOT})
-if(LINUX)
-    list(APPEND MKL_HINTS "/opt/intel/oneapi/mkl/latest")
-endif()
-if(WIN32)
-    list(APPEND MKL_HINTS "C:/Program Files (x86)/Intel/oneAPI/mkl/latest")
-endif()
-find_package(MKL HINTS ${MKL_HINTS})
-if(NOT MKL_FOUND)
-    message(WARNING "MKL not found - disabling SVS shared library")
+if(USE_SVS)
+    message(STATUS "SVS support enabled")
+    # try to find MKL
+    set(MKL_HINTS ${MKLROOT} $ENV{MKL_DIR} $ENV{MKL_ROOT} $ENV{MKLROOT})
+    if(LINUX)
+        list(APPEND MKL_HINTS "/opt/intel/oneapi/mkl/latest")
+    endif()
+    if(WIN32)
+        list(APPEND MKL_HINTS "C:/Program Files (x86)/Intel/oneAPI/mkl/latest")
+    endif()
+    find_package(MKL HINTS ${MKL_HINTS})
+    if(NOT MKL_FOUND)
+        message(WARNING "MKL not found - disabling SVS shared library")
+    endif()
+else()
+    message(STATUS "SVS support disabled")
+    set(MKL_FOUND FALSE)
 endif()
 
 # Configure SVS build
-cmake_dependent_option(SVS_SHARED_LIB "Use SVS pre-compiled shared library" ON "SVS_SUPPORTED AND MKL_FOUND" OFF)
+cmake_dependent_option(SVS_SHARED_LIB "Use SVS pre-compiled shared library" ON "USE_SVS AND MKL_FOUND" OFF)
 set(SVS_URL "https://github.com/intel/ScalableVectorSearch/releases/download/v0.0.7/svs-shared-library-0.0.7-avx2.tar.gz" CACHE STRING "SVS URL")
 
 if(USE_SVS)
@@ -76,8 +75,8 @@ if(USE_SVS)
         set(SVS_LVQ_HEADER "svs/extensions/vamana/lvq.h")
         set(SVS_TARGET_NAME "svs::svs_shared_library")
     else()
-        # this file is included from python_bindings/CMakeLists.txt
-        # SVS sources path to be relative to this file
+        # this file is included from both CMakeLists.txt and python_bindings/CMakeLists.txt
+        # so, set SVS sources path relative to this file
         get_filename_component(root ${CMAKE_CURRENT_LIST_DIR}/.. ABSOLUTE)
         add_subdirectory(
             ${root}/deps/ScalableVectorSearch
