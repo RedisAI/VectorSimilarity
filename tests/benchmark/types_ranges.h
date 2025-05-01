5e1089e97efc8b1cbd7ffd1bf8d2970e97cffb64
DvirDukhan <dvir@redis.com>
2025-05-01 17:19:11 +0300

    format

--- tests/benchmark/types_ranges.h
+++ tests/benchmark/types_ranges.h
@@ -5,16 +5,14 @@
  * Licensed under your choice of the Redis Source Available License 2.0
  * (RSALv2); or (b) the Server Side Public License v1 (SSPLv1); or (c) the
  * GNU Affero General Public License v3 (AGPLv3).
-*/
+ */
 
 #pragma once
 #include <array>
 #include "bm_definitions.h"
 
-#define DEFAULT_RANGE_RADII                                                                        \
-    { 20, 35, 50 }
-#define DEFAULT_RANGE_EPSILONS                                                                     \
-    { 1, 10, 11 }
+#define DEFAULT_RANGE_RADII    {20, 35, 50}
+#define DEFAULT_RANGE_EPSILONS {1, 10, 11}
 
 // This template struct methods returns the default values for radii and epsilons
 // To specify different values for a certain type, use template specialization
@@ -26,8 +24,7 @@ struct benchmark_range {
 
 // Larger Range query values are required for int8 wikipedia dataset.
 // Default values give 0 results
-#define INT8_RANGE_RADII                                                                           \
-    { 50, 65, 80 }
+#define INT8_RANGE_RADII {50, 65, 80}
 
 template <>
 struct benchmark_range<int8_index_t> {
@@ -36,8 +33,7 @@ struct benchmark_range<int8_index_t> {
 };
 
 // UINT8 ranges
-#define UINT8_RANGE_RADII                                                                          \
-    { 4, 5, 7 }
+#define UINT8_RANGE_RADII {4, 5, 7}
 template <>
 struct benchmark_range<uint8_index_t> {
     static std::array<unsigned int, 3> get_radii() { return UINT8_RANGE_RADII; }
--- tests/unit/test_allocator.cpp
+++ tests/unit/test_allocator.cpp
@@ -5,7 +5,7 @@
  * Licensed under your choice of the Redis Source Available License 2.0
  * (RSALv2); or (b) the Server Side Public License v1 (SSPLv1); or (c) the
  * GNU Affero General Public License v3 (AGPLv3).
-*/
+ */
 
 #include "gtest/gtest.h"
 #include "VecSim/vec_sim.h"
@@ -33,7 +33,7 @@ struct ObjectWithSTL : public VecsimBaseObject {
 
 public:
     ObjectWithSTL(std::shared_ptr<VecSimAllocator> allocator)
-        : VecsimBaseObject(allocator), test_vec(allocator){};
+        : VecsimBaseObject(allocator), test_vec(allocator) {};
 };
 
 struct NestedObject : public VecsimBaseObject {
@@ -42,7 +42,7 @@ struct NestedObject : public VecsimBaseObject {
 
 public:
     NestedObject(std::shared_ptr<VecSimAllocator> allocator)
-        : VecsimBaseObject(allocator), stl_object(allocator), simpleObject(allocator){};
+        : VecsimBaseObject(allocator), stl_object(allocator), simpleObject(allocator) {};
 };
 
 TEST_F(AllocatorTest, test_simple_object) {
