13f21662367c8f4ad53ea4399128cb0d4af5e44f
nafraf <nafraf@gmail.com>
2025-05-01 10:43:16 -0500

    Fix format

--- tests/benchmark/run_files/bm_basics_single_fp32.cpp
+++ tests/benchmark/run_files/bm_basics_single_fp32.cpp
@@ -1,12 +1,11 @@
 /*
-* Copyright (c) 2006-Present, Redis Ltd.
-* All rights reserved.
-*
-* Licensed under your choice of the Redis Source Available License 2.0
-* (RSALv2); or (b) the Server Side Public License v1 (SSPLv1); or (c) the
-* GNU Affero General Public License v3 (AGPLv3).
-*/
-
+ * Copyright (c) 2006-Present, Redis Ltd.
+ * All rights reserved.
+ *
+ * Licensed under your choice of the Redis Source Available License 2.0
+ * (RSALv2); or (b) the Server Side Public License v1 (SSPLv1); or (c) the
+ * GNU Affero General Public License v3 (AGPLv3).
+ */
 
 #include "benchmark/bm_vecsim_basics.h"
 #include "VecSim/algorithms/brute_force/brute_force_single.h"
