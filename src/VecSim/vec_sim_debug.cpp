904f795f2c7d923d845c115b78f7beb77b295285
nafraf <nafraf@gmail.com>
2025-05-01 10:51:42 -0500

    Revert "Fix format"
    This reverts commit 9ce78fc01cdd512c7a32d7cd46cd2a06772bb003.

--- src/VecSim/vec_sim_debug.cpp
+++ src/VecSim/vec_sim_debug.cpp
@@ -5,7 +5,7 @@
  * Licensed under your choice of the Redis Source Available License 2.0
  * (RSALv2); or (b) the Server Side Public License v1 (SSPLv1); or (c) the
  * GNU Affero General Public License v3 (AGPLv3).
- */
+*/
 
 #include "vec_sim_debug.h"
 #include "VecSim/vec_sim_index.h"
