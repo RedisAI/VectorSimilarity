bf54c4c0d2a2313f55d90f49f8df9d8d597ab6ed
Rafik Saliev <rafik.f.saliev@intel.com>
2025-05-14 10:52:02 +0200

    Fix 'unused variable` build error

--- src/VecSim/algorithms/svs/svs_tiered.h
+++ src/VecSim/algorithms/svs/svs_tiered.h
@@ -538,9 +538,7 @@ private:
         { // lock both indicies for writing - these changes to be synchronized
             std::scoped_lock lock(this->flatIndexGuard, this->mainIndexGuard);
             auto svs_index = GetSVSIndex();
-            auto deleted_num =
-                svs_index->deleteVectors(labels_to_delete.data(), labels_to_delete.size());
-            assert(deleted_num == 0);
+            svs_index->deleteVectors(labels_to_delete.data(), labels_to_delete.size());
             assert(labels_to_add.size() == vectors_to_add.size() / this->frontendIndex->getDim());
             svs_index->addVectors(vectors_to_add.data(), labels_to_add.data(),
                                   labels_to_add.size());
