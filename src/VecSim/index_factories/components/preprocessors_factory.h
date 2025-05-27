0929890d4fc1d4899443a2784045fa8febd472ae
meiravgri <109056284+meiravgri@users.noreply.github.com>
2025-05-27 14:27:16 +0300

    [MOD-9576] Fix memory safety in cosine preprocessing (#670)
    * make CreatePreprocessorsContainerParams templated and move it to header file
    
    add logic to determine the cosine processed_bytes_count
    
    add processed_bytes_count to the cosine component
    
    change blob_size param to the original blob size instead of processed_bytes_count in all preprocessing functions.
    
    * plan for the tests
    
    * rename original_blob_size-> input_blob_size
    
    add DummyChangeAllocSizePreprocessor class to test pp that changes the size
    
    add tests
    one will fail because the input blob size is not updated
    
    * preprocessors now change the blob size
    
    * fix test
    
    * fix tiered test
    
    * add assert storage_blob == nullptr || input_blob_size == processed_bytes_count
    
    add valgrind macro
    
    value_to_add of DummyQueryPreprocessor and DummyMixedPreprocessor is now DataType instead of int
    
    * enable assert only in debug
    
    * use constexpr for blob size
    
    * small docs changes
    
    * review fixes
    
    * ש

--- src/VecSim/index_factories/components/preprocessors_factory.h
+++ src/VecSim/index_factories/components/preprocessors_factory.h
@@ -15,6 +15,7 @@ struct PreprocessorsContainerParams {
     VecSimMetric metric;
     size_t dim;
     unsigned char alignment;
+    size_t processed_bytes_count;
 };
 
 template <typename DataType>
@@ -25,8 +26,8 @@ CreatePreprocessorsContainer(std::shared_ptr<VecSimAllocator> allocator,
     if (params.metric == VecSimMetric_Cosine) {
         auto multiPPContainer =
             new (allocator) MultiPreprocessorsContainer<DataType, 1>(allocator, params.alignment);
-        auto cosine_preprocessor =
-            new (allocator) CosinePreprocessor<DataType>(allocator, params.dim);
+        auto cosine_preprocessor = new (allocator)
+            CosinePreprocessor<DataType>(allocator, params.dim, params.processed_bytes_count);
         int next_valid_pp_index = multiPPContainer->addPreprocessor(cosine_preprocessor);
         UNUSED(next_valid_pp_index);
         assert(next_valid_pp_index != -1 && "Cosine preprocessor was not added correctly");
