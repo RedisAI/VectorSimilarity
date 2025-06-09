1eb51be480472555ad73fb8c51877c43000cff86
alon <alonreshef24@gmail.com>
2025-06-09 12:06:03 +0300

    Install latest clang-18 version in ci

--- .install/ubuntu_24.04.sh
+++ .install/ubuntu_24.04.sh
@@ -4,6 +4,14 @@ export DEBIAN_FRONTEND=noninteractive
 MODE=$1 # whether to install using sudo or not
 
 $MODE apt update -qq
+sudo apt install -y wget lsb-release gnupg
+
+# Add the LLVM APT repo for the latest clang-18 version
+wget https://apt.llvm.org/llvm.sh
+chmod +x llvm.sh
+$MODE ./llvm.sh 18
+clang-format-18 --version
+
 $MODE apt install -yqq git wget build-essential lcov openssl libssl-dev \
-    python3 python3-venv python3-dev unzip rsync clang curl
+    python3 python3-venv python3-dev unzip rsync clang curl clang-format
 source install_cmake.sh $MODE
