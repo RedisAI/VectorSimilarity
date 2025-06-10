466f42bb19443e0d79e58dd65af045385bff81ca
Dor Forer <dor.forer@redis.com>
2025-06-10 15:41:48 +0300

    format

--- tests/benchmark/types_ranges.h
+++ tests/benchmark/types_ranges.h
@@ -11,9 +11,11 @@
 #include <array>
 #include "bm_definitions.h"
 
-#define DEFAULT_RANGE_RADII    {20, 35, 50}
+#define DEFAULT_RANGE_RADII                                                                        \
+    { 20, 35, 50 }
 
-#define DEFAULT_RANGE_EPSILONS {1, 10, 11}
+#define DEFAULT_RANGE_EPSILONS                                                                     \
+    { 1, 10, 11 }
 
 // This template struct methods returns the default values for radii and epsilons
 // To specify different values for a certain type, use template specialization
@@ -25,8 +27,8 @@ struct benchmark_range {
 
 // Larger Range query values are required for int8 wikipedia dataset.
 // Default values give 0 results
-#define INT8_RANGE_RADII {50, 65, 80}
-
+#define INT8_RANGE_RADII                                                                           \
+    { 50, 65, 80 }
 
 template <>
 struct benchmark_range<int8_index_t> {
@@ -35,7 +37,8 @@ struct benchmark_range<int8_index_t> {
 };
 
 // UINT8 ranges
-#define UINT8_RANGE_RADII {4, 5, 7}
+#define UINT8_RANGE_RADII                                                                          \
+    { 4, 5, 7 }
 
 template <>
 struct benchmark_range<uint8_index_t> {
