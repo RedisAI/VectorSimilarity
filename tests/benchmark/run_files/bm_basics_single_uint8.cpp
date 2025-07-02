2e871bb387bf50e330f2799bec7b678d5f641f5d
dor-forer <dor.forer@redis.com>
2025-07-02 11:22:42 +0300

    Generalize Micro-benchmarks  [MOD-10160]  (#703)
    * add array and index
    
    * add docs
    fix "$BM_TYPE" = "bm-updated-fp32-single"
    
    * unify Memory benchmarks function to take index type parameter: Replace Memory_FLAT/HNSW/Tiered with single Memory function taking IndexTypeIndex to reduce code duplication.
    
    replace INDEX_TYPE_SVS_QUANTIZED with INDEX_TYPE_SVS_COMPRESSED
    
    * refactor: improve index ownership management in benchmarks
    - Replaced manual reference counting with `IndexPtr` for safer and simpler ownership management.
    - Introduced `IndexPtr` as a smart pointer wrapper for `VecSimIndex` to:
      1. Ensure proper cleanup using RAII principles.
      2. Enable safe ownership transfer via `release_ownership()`.
      3. Support shared ownership with thread pool using `get_shared()`.
    
    introducee GET_INDEX to access an index in the indices array
    
    refactor: improve benchmark ownership and initialization logic
    
    - Ensured one-time initialization using `is_initialized` flags.
    
    * RunTopK_HNSW: remove unused is_tiered
    
    rename IndexTypeFlags etries from INDEX_TYPE to INDEX_MASK
    
    initialize threadpool mock only if needed
    
    * initizlie bf after hnsw
    
    * fix mockethread pool deletion
    
    * introduce DEFAULT_BM_INDEXES_MASK that defines bf, hnsw and tierred
    
    address CR
    
    ---------
    
    Co-authored-by: meiravgri <meirav.grimberg@redis.com>
    (cherry picked from commit 44acb877eb1a62f03c4d4300eaaa7fdf58338694)

--- tests/benchmark/run_files/bm_basics_single_uint8.cpp
+++ tests/benchmark/run_files/bm_basics_single_uint8.cpp
@@ -7,13 +7,15 @@
 ***************************************/
 
 bool BM_VecSimGeneral::is_multi = false;
+uint32_t BM_VecSimGeneral::enabled_index_types = IndexTypeFlags::INDEX_MASK_BF |
+                                                 IndexTypeFlags::INDEX_MASK_HNSW |
+                                                 IndexTypeFlags::INDEX_MASK_TIERED_HNSW;
 
 size_t BM_VecSimGeneral::n_queries = 10000;
 size_t BM_VecSimGeneral::n_vectors = 1000000;
 size_t BM_VecSimGeneral::dim = 1024;
 size_t BM_VecSimGeneral::M = 64;
 size_t BM_VecSimGeneral::EF_C = 512;
-tieredIndexMock BM_VecSimGeneral::mock_thread_pool{};
 
 const char *BM_VecSimGeneral::hnsw_index_file =
     "tests/benchmark/data/wipedia_single_uint8-cosine-dim1024-M64-efc512-uint8.hnsw_v3";
@@ -26,11 +28,11 @@ const char *BM_VecSimGeneral::test_queries_file =
 #define BM_DELETE_LABEL_ASYNC       DeleteLabel_Async_Single
 
 DEFINE_DELETE_LABEL(BM_FUNC_NAME(DeleteLabel, BF), uint8_index_t, BruteForceIndex_Single, uint8_t,
-                    float, VecSimAlgo_BF)
+                    float, INDEX_BF)
 DEFINE_DELETE_LABEL(BM_FUNC_NAME(DeleteLabel, HNSW), uint8_index_t, HNSWIndex_Single, uint8_t,
-                    float, VecSimAlgo_HNSWLIB)
+                    float, INDEX_HNSW)
 DEFINE_DELETE_LABEL(BM_FUNC_NAME(DeleteLabel, Tiered), uint8_index_t, TieredHNSWIndex, uint8_t,
-                    float, VecSimAlgo_TIERED)
+                    float, INDEX_TIERED_HNSW)
 #include "benchmark/bm_initialization/bm_basics_initialize_uint8.h"
 
 BENCHMARK_MAIN();
--- tests/benchmark/run_files/bm_batch_iterator_multi_uint8.cpp
+++ tests/benchmark/run_files/bm_batch_iterator_multi_uint8.cpp
@@ -1,6 +1,7 @@
 #include "benchmark/bm_batch_iterator.h"
 
 bool BM_VecSimGeneral::is_multi = true;
+uint32_t BM_VecSimGeneral::enabled_index_types = DEFAULT_BM_INDEXES_MASK;
 
 size_t BM_VecSimGeneral::n_queries = 10000;
 size_t BM_VecSimGeneral::n_vectors = 1000000;
@@ -8,7 +9,6 @@ size_t BM_VecSimGeneral::dim = 1024;
 size_t BM_VecSimGeneral::M = 64;
 size_t BM_VecSimGeneral::EF_C = 512;
 size_t BM_VecSimGeneral::block_size = 1024;
-tieredIndexMock BM_VecSimGeneral::mock_thread_pool{};
 
 const char *BM_VecSimGeneral::hnsw_index_file =
     "tests/benchmark/data/wipedia_multi_uint8-cosine-dim1024-M64-efc512-uint8.hnsw_v3";
