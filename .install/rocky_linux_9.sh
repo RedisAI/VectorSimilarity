4c3256bf7df908bda5bd0e4b227fccd72bb4360a
meiravgri <meirav.grimberg@redis.com>
2025-07-02 04:26:40 +0000

    align rocky9 with search

--- .install/rocky_linux_9.sh
+++ .install/rocky_linux_9.sh
@@ -3,7 +3,7 @@ MODE=$1 # whether to install using sudo or not
 set -e
 export DEBIAN_FRONTEND=noninteractive
 $MODE dnf update -y
-$MODE dnf install -y gcc-toolset-13-gcc gcc-toolset-13-gcc-c++ make wget git valgrind
+$MODE dnf install -y gcc-toolset-13-gcc gcc-toolset-13-gcc-c++ make wget git valgrind --nobest --skip-broken --allowerasing
 
 cp /opt/rh/gcc-toolset-13/enable /etc/profile.d/gcc-toolset-13.sh
 
