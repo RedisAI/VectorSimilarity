4bbd83bb0d915bfb7205ec82b23fde5cd1097932
lerman25 <lerman25@gmail.com>
2025-08-20 18:52:54 +0300

    change alpine install script

--- .install/alpine_linux_3.sh
+++ .install/alpine_linux_3.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
-MODE=$1
+MODE=$1 # whether to install using sudo or not
 set -e
 
 $MODE apk update
-$MODE apk add --no-cache build-base gcc g++ make wget git valgrind linux-headers cmake
 
-# Force C++17 and prevent the C++20 deprecation from being a hard error
-export CXXFLAGS="$CXXFLAGS -std=gnu++17 -Wno-error=deprecated -Wno-error=deprecated-declarations"
-export CFLAGS="$CFLAGS -std=gnu++17"
+# pin GCC/G++ to 13 (avoid unversioned gcc/g++ from build-base)
+$MODE apk add --no-cache make binutils musl-dev gcc-13 g++-13 wget git valgrind linux-headers
 
-# (Optional) persist for subsequent CI steps/shells
-echo 'export CXXFLAGS="$CXXFLAGS -std=gnu++17 -Wno-error=deprecated -Wno-error=deprecated-declarations"' | $MODE tee /etc/profile.d/cpp17.sh >/dev/null
-echo 'export CFLAGS="$CFLAGS -std=gnu++17"' | $MODE tee -a /etc/profile.d/cpp17.sh >/dev/null
+$MODE apk add --no-cache cmake
