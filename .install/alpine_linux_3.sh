457faa17224e86d34464537b9d5b52c10d3bc24a
lerman25 <lerman25@gmail.com>
2025-08-21 10:34:57 +0300

    Revert "change alpine install script"
    This reverts commit aa493e5b4b396f629b30fd88ceb73dc2ea890fdf.

--- .install/alpine_linux_3.sh
+++ .install/alpine_linux_3.sh
@@ -4,7 +4,6 @@ set -e
 
 $MODE apk update
 
-# pin GCC/G++ to 13 (avoid unversioned gcc/g++ from build-base)
-$MODE apk add --no-cache make binutils musl-dev gcc-13 g++-13 wget git valgrind linux-headers
+$MODE apk add --no-cache build-base gcc g++ make wget git valgrind linux-headers
 
 $MODE apk add --no-cache cmake
