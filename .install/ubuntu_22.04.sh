ba3d561e7b3ee342acfa35dde4d1b4f0e16dab99
meiravgri <meirav.grimberg@redis.com>
2025-10-15 05:23:38 +0000

    install clang

--- .install/ubuntu_22.04.sh
+++ .install/ubuntu_22.04.sh
@@ -4,7 +4,7 @@ export DEBIAN_FRONTEND=noninteractive
 MODE=$1 # whether to install using sudo or not
 
 $MODE apt-get update -qq || true
-$MODE apt-get install -yqq gcc-12 g++-12 git wget build-essential valgrind lcov
+$MODE apt-get install -yqq gcc-12 g++-12 git wget build-essential clang  lcov
 $MODE update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-12 60 --slave /usr/bin/g++ g++ /usr/bin/g++-12
 # align gcov version with gcc version
 $MODE update-alternatives --install /usr/bin/gcov gcov /usr/bin/gcov-12 60
