17c66ea98a0aa8b0acbb10dcf6bcc4974711b0d0
meiravgri <meirav.grimberg@redis.com>
2025-10-15 07:34:51 +0000

    install llvm on cov

--- .install/ubuntu_22.04.sh
+++ .install/ubuntu_22.04.sh
@@ -8,14 +8,4 @@ $MODE apt-get install -yqq gcc-12 g++-12 git wget build-essential lcov
 $MODE update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-12 60 --slave /usr/bin/g++ g++ /usr/bin/g++-12
 # align gcov version with gcc version
 $MODE update-alternatives --install /usr/bin/gcov gcov /usr/bin/gcov-12 60
-
-# Add Clang 18 installation
-# Download and run the official LLVM installation script
-wget https://apt.llvm.org/llvm.sh
-chmod +x llvm.sh
-$MODE ./llvm.sh 18
-$MODE apt-get install -yqq clang-18 clang++-18
-$MODE update-alternatives --install /usr/bin/clang clang /usr/bin/clang-18 100 --slave /usr/bin/clang++ clang++ /usr/bin/clang++-18
-
-echo `clang --version`
 source install_cmake.sh $MODE
