a2dad0c5683b92462d40607510feb9a2d879047b
meiravgri <meirav.grimberg@redis.com>
2025-11-04 15:21:48 +0000

    sepearate non compression and compression bm

--- tests/benchmark/bm_initialization/bm_training_initialize_fp32.h
+++ tests/benchmark/bm_initialization/bm_training_initialize_fp32.h
@@ -16,22 +16,39 @@ the file.
 #define UNIT_AND_ITERATIONS Unit(benchmark::kMillisecond)->Iterations(5)
 
 #ifdef HAVE_SVS_LVQ
-#define QUANT_BITS_ARGS {VecSimSvsQuant_NONE, VecSimSvsQuant_8, VecSimSvsQuant_4x8_LeanVec}
+#define QUANT_BITS_ARGS {VecSimSvsQuant_8, VecSimSvsQuant_4x8_LeanVec}
 #else
-#define QUANT_BITS_ARGS {VecSimSvsQuant_NONE, VecSimSvsQuant_8}
+#define QUANT_BITS_ARGS {VecSimSvsQuant_8}
 #endif
 
-BENCHMARK_TEMPLATE_DEFINE_F(BM_VecSimSVSTrain, BM_Train, fp32_index_t)
+BENCHMARK_TEMPLATE_DEFINE_F(BM_VecSimSVSTrain, BM_TrainNoCompression, fp32_index_t)
 (benchmark::State &st) { Train(st); }
-BENCHMARK_REGISTER_F(BM_VecSimSVSTrain, BM_Train)
+BENCHMARK_REGISTER_F(BM_VecSimSVSTrain, BM_TrainNoCompression)
+    ->UNIT_AND_ITERATIONS
+    ->Args({VecSimSvsQuant_NONE, static_cast<long int>(BM_VecSimGeneral::block_size)})
+    ->ArgNames({"quant_bits", "training_threshold"});
+
+BENCHMARK_TEMPLATE_DEFINE_F(BM_VecSimSVSTrain, BM_TrainAsyncNoCompression, fp32_index_t)
+(benchmark::State &st) { TrainAsync(st); }
+BENCHMARK_REGISTER_F(BM_VecSimSVSTrain, BM_TrainAsyncNoCompression)
+    ->UNIT_AND_ITERATIONS
+    ->ArgsProduct({{VecSimSvsQuant_NONE},
+                   {static_cast<long int>(BM_VecSimGeneral::block_size)},
+                   {4, 8, 16}})
+    ->ArgNames({"quant_bits", "training_threshold", "thread_count"})
+    ->MeasureProcessCPUTime();
+
+BENCHMARK_TEMPLATE_DEFINE_F(BM_VecSimSVSTrain, BM_TrainCompressed, fp32_index_t)
+(benchmark::State &st) { Train(st); }
+BENCHMARK_REGISTER_F(BM_VecSimSVSTrain, BM_TrainCompressed)
     ->UNIT_AND_ITERATIONS
     ->ArgsProduct({QUANT_BITS_ARGS,
                    {static_cast<long int>(BM_VecSimGeneral::block_size), 5000, 10000}})
     ->ArgNames({"quant_bits", "training_threshold"});
 
-BENCHMARK_TEMPLATE_DEFINE_F(BM_VecSimSVSTrain, BM_TrainAsync, fp32_index_t)
+BENCHMARK_TEMPLATE_DEFINE_F(BM_VecSimSVSTrain, BM_TrainCompressedAsync, fp32_index_t)
 (benchmark::State &st) { TrainAsync(st); }
-BENCHMARK_REGISTER_F(BM_VecSimSVSTrain, BM_TrainAsync)
+BENCHMARK_REGISTER_F(BM_VecSimSVSTrain, BM_TrainCompressedAsync)
     ->UNIT_AND_ITERATIONS
     ->ArgsProduct({QUANT_BITS_ARGS,
                    {static_cast<long int>(BM_VecSimGeneral::block_size), 5000, 10000, 50000,
