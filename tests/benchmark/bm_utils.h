1a15b598562d8dca01fcedd85df2c1ca36f394df
meiravgri <109056284+meiravgri@users.noreply.github.com>
2025-11-10 15:32:01 +0200

    [MOD-10238] Training bm for SVS (#822)
    * initial imp of training bm
    
    introduce BM_VecSimSVSTrain class with 2 methods: Train and TrainAsync
    
    add GoogleTest to benchmarks so we can use ASSERT_* API
    
    tieredIndexMock: possible to initialize with a specific thread count
    
    add train bemchmark to CI benchmark dispatcher
    
    * make bm_files general for other algs
    
    rename svs_training_fp32 ->svs_indices_training_fp32
    
    add to bm_files.sh
    
    * replace std::formtat only supported from gcc13 with ostringstream
    
    * format
    
    * revrt assert
    
    * intialize quantBits
    move svs params init to CreateTieredSVSIndex
    
    only 5 iterations
    
    * move iterawtion logic to runTrainBMIteration
    
    add compressed index bm
    
    * assert depdnding on HAVE_SVS_LVQ
    
    * sepearate non compression and compression bm
    
    * TO REVERT !!! test abort
    
    * fix if else
    
    * revrt timeoutgurard vhanges
    
    * dont pause after training to see how it affrects performance
    
    remove some prints
    
    * fix #ifdef HAVE_SVS_LVQ to #if HAVE_SVS_LVQ
    
    * use pause timers its faster
    
    * do 3 iter instread of 5 and test if results are stable
    
    * use 5 again
    
    * fix download all all script
    
    * fp16 bm
    
    remove 100K
    
    * remove 100K from fp32
    
    * increase timeout
    
    * try bigger machine
    
    * try a bigger machine
    
    * try 2 iter
    
    move UNIT_AND_ITERATIONS and QUANT_BITS_ARGS to bm_vecsim_basics_Svs
    
    * unify bm_training_initialize_fp32.h and bm_training_initialize_fp16.h to bm_training_initialize.h
    
    define DATA_TYPE_INDEX_T in bm_svs_training_fp*.cpp
    
    remove th 10k and 50k for arm
    
    * reevet timeout to 10
    
    fix include header for fp16
    
    * move CreateTieredSVSParams and verifyNumThreads to svs params
    
    * revert increease machine size
    
    * change assert to log
    
    * fix
    
    * fix2
    
    * format
    
    * rename BM_VecSimSVSTrain->BM_VecSimSVS
    
    bm_vecsim_svs_train.h->bm_vecsim_svs
    
    * remove unrelated
    
    * Modify mock_thread_pool initialization in Train benchmark
    
    * improve comment

---
+++ tests/benchmark/bm_utils.h
@@ -0,0 +1,41 @@
+/*
+ * Copyright (c) 2006-Present, Redis Ltd.
+ * All rights reserved.
+ *
+ * Licensed under your choice of the Redis Source Available License 2.0
+ * (RSALv2); or (b) the Server Side Public License v1 (SSPLv1); or (c) the
+ * GNU Affero General Public License v3 (AGPLv3).
+ */
+#pragma once
+
+#include "VecSim/index_factories/tiered_factory.h"
+#include "utils/mock_thread_pool.h"
+#include "VecSim/algorithms/svs/svs_utils.h"
+
+namespace test_utils {
+static TieredIndexParams
+CreateTieredSVSParams(VecSimParams &svs_params, tieredIndexMock &mock_thread_pool,
+                      size_t training_threshold, size_t update_threshold,
+                      size_t update_job_wait_time = SVS_DEFAULT_UPDATE_JOB_WAIT_TIME) {
+    return TieredIndexParams{.jobQueue = &mock_thread_pool.jobQ,
+                             .jobQueueCtx = mock_thread_pool.ctx,
+                             .submitCb = tieredIndexMock::submit_callback,
+                             .primaryIndexParams = &svs_params,
+                             .specificParams = {.tieredSVSParams = TieredSVSParams{
+                                                    .trainingTriggerThreshold = training_threshold,
+                                                    .updateTriggerThreshold = update_threshold,
+                                                    .updateJobWaitTime = update_job_wait_time}}};
+}
+
+template <typename data_t>
+static void verifyNumThreads(TieredSVSIndex<data_t> *tiered_index, size_t expected_num_threads,
+                             size_t expected_capcity) {
+    ASSERT_EQ(tiered_index->GetSVSIndex()->getThreadPoolCapacity(), expected_capcity)
+        << "thread pool capacity mismatch";
+    size_t num_reserved_threads = tiered_index->GetSVSIndex()->getNumThreads();
+    if (num_reserved_threads < expected_num_threads) {
+        std::cout << "WARNING: last reserved threads (" << num_reserved_threads
+                  << ") is less than expected (" << expected_num_threads << ")." << std::endl;
+    }
+}
+} // namespace test_utils
