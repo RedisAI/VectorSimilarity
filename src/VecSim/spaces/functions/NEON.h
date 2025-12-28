d0fd2e493fe24ee267f37c084112b40745f1d75d
Dor Forer <dor.forer@redis.com>
2025-12-28 16:30:22 +0200

    Remove NEON DOTPROD-optimized distance functions for INT8, UINT8, and SQ8-to-SQ8 calculations

--- src/VecSim/spaces/functions/NEON.h
+++ src/VecSim/spaces/functions/NEON.h
@@ -34,17 +34,4 @@ dist_func_t<float> Choose_SQ8_Cosine_implementation_NEON(size_t dim);
 dist_func_t<float> Choose_SQ8_SQ8_IP_implementation_NEON(size_t dim);
 dist_func_t<float> Choose_SQ8_SQ8_Cosine_implementation_NEON(size_t dim);
 
-// NEON DOTPROD-optimized functions
-dist_func_t<float> Choose_INT8_IP_implementation_NEON_DOTPROD(size_t dim);
-dist_func_t<float> Choose_INT8_Cosine_implementation_NEON_DOTPROD(size_t dim);
-dist_func_t<float> Choose_INT8_L2_implementation_NEON_DOTPROD(size_t dim);
-
-dist_func_t<float> Choose_UINT8_IP_implementation_NEON_DOTPROD(size_t dim);
-dist_func_t<float> Choose_UINT8_Cosine_implementation_NEON_DOTPROD(size_t dim);
-dist_func_t<float> Choose_UINT8_L2_implementation_NEON_DOTPROD(size_t dim);
-
-// SQ8-to-SQ8 DOTPROD-optimized distance functions
-dist_func_t<float> Choose_SQ8_SQ8_IP_implementation_NEON_DOTPROD(size_t dim);
-dist_func_t<float> Choose_SQ8_SQ8_Cosine_implementation_NEON_DOTPROD(size_t dim);
-
 } // namespace spaces
