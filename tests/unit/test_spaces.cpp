e50dc45a3149743ec61d01d131be48fd37d69650
Dor Forer <dor.forer@redis.com>
2026-01-04 09:13:18 +0200

    Add CPU feature checks to disable optimizations for AArch64 in SQ8 distance function tests

--- tests/unit/test_spaces.cpp
+++ tests/unit/test_spaces.cpp
@@ -2356,7 +2356,14 @@ TEST_F(SpacesTest, SQ8_SQ8_ip_no_optimization_func_test) {
                                                                    v2_quantized.data(), dim);
 
     unsigned char alignment = 0;
+    #ifdef CPU_FEATURES_ARCH_AARCH64
+    // Make sure we don't use any optimization (because there is no size optimization for arm)
+    auto optimization = getCpuOptimizationFeatures();
+    optimization.sve = optimization.sve2 = optimization.asimddp = optimization.asimd = 0;
+    auto arch_opt_func = IP_SQ8_SQ8_GetDistFunc(dim, &alignment, &optimization);
+    #else
     auto arch_opt_func = IP_SQ8_SQ8_GetDistFunc(dim, &alignment, nullptr);
+    #endif
     ASSERT_EQ(arch_opt_func, SQ8_SQ8_InnerProduct)
         << "Unexpected distance function chosen for dim " << dim;
     // Checks that the function with the optimzied math equivalence returns the same result.
@@ -2379,7 +2386,14 @@ TEST_F(SpacesTest, SQ8_SQ8_Cosine_no_optimization_func_test) {
         test_utils::SQ8_SQ8_NotOptimized_Cosine(v1_quantized.data(), v2_quantized.data(), dim);
 
     unsigned char alignment = 0;
+    #ifdef CPU_FEATURES_ARCH_AARCH64
+    // Make sure we don't use any optimization (because there is no size optimization for arm)
+    auto optimization = getCpuOptimizationFeatures();
+    optimization.sve = optimization.sve2 = optimization.asimddp = optimization.asimd = 0;
+    auto arch_opt_func = Cosine_SQ8_SQ8_GetDistFunc(dim, &alignment, &optimization);
+    #else
     auto arch_opt_func = Cosine_SQ8_SQ8_GetDistFunc(dim, &alignment, nullptr);
+    #endif
     ASSERT_EQ(arch_opt_func, SQ8_SQ8_Cosine)
         << "Unexpected distance function chosen for dim " << dim;
     // Checks that the function with the optimzied math equivalence returns the same result.
