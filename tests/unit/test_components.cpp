1995cebe94469c50182562ebb5e8283863de8ecb
Dor Forer <dor.forer@redis.com>
2026-07-22 18:21:59 +0300

    Remove ARM copyright headers from modified files

--- tests/unit/test_components.cpp
+++ tests/unit/test_components.cpp
@@ -1,8 +1,6 @@
 /*
  * Copyright (c) 2006-Present, Redis Ltd.
  * All rights reserved.
- * SPDX-FileCopyrightText: Copyright 2026 Arm Limited and/or its affiliates
- * <open-source-office@arm.com>
  *
  * Licensed under your choice of the Redis Source Available License 2.0
  * (RSALv2); or (b) the Server Side Public License v1 (SSPLv1); or (c) the
